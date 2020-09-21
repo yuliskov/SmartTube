311cd4998d6192d1ab824ebf194f0691e68edefc
Yuriy Liskov <firsthash@gmail.com>
2020-09-21 21:20:59 +0300

    channels: first step

--- smartyoutubetv2/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/main/SplashActivity.java
+++ smartyoutubetv2/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/main/SplashActivity.java
@@ -1,10 +1,15 @@
 package com.liskovsoft.smartyoutubetv2.tv.ui.main;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import com.liskovsoft.sharedutils.mylogger.Log;
 import com.liskovsoft.smartyoutubetv2.common.app.views.ViewManager;
 
 public class SplashActivity extends Activity {
+    private static final String TAG = SplashActivity.class.getSimpleName();
+    private static final String CHANNELS_RECEIVER_CLASS_NAME = "com.liskovsoft.leanbackassistant.channels.RunOnInstallReceiver";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,12 +17,26 @@ public class SplashActivity extends Activity {
         ViewManager viewManager = ViewManager.instance(this);
         viewManager.startDefaultView(this);
 
+        updateChannels();
+
         finish();
     }
 
-    // TODO: implement Android TV channels
-    //private void updateRecommendations() {
-    //    Intent recommendationIntent = new Intent(mContext, UpdateRecommendationsService.class);
-    //    mContext.startService(recommendationIntent);
-    //}
+    private void updateChannels() {
+        Class<?> clazz = null;
+
+        try {
+            clazz = Class.forName(CHANNELS_RECEIVER_CLASS_NAME);
+        } catch (ClassNotFoundException e) {
+            // NOP
+        }
+
+        if (clazz != null) {
+            Log.d(TAG, "Starting channels receiver...");
+            Intent intent = new Intent(this, clazz);
+            sendBroadcast(intent);
+        } else {
+            Log.e(TAG, "Channels receiver class not found: " + CHANNELS_RECEIVER_CLASS_NAME);
+        }
+    }
 }
