1712ed66cb844a33dff7dd08784607b589c39739
Yuriy Liskov <firsthash@gmail.com>
2020-09-22 05:41:42 +0300

    history fix

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/managers/HistoryUpdater.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/managers/HistoryUpdater.java
@@ -6,6 +6,7 @@ import com.liskovsoft.sharedutils.mylogger.Log;
 import com.liskovsoft.smartyoutubetv2.common.app.models.data.Video;
 import com.liskovsoft.smartyoutubetv2.common.app.models.playback.PlayerEventListenerHelper;
 import com.liskovsoft.youtubeapi.service.YouTubeMediaService;
+import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
@@ -15,10 +16,24 @@ public class HistoryUpdater extends PlayerEventListenerHelper {
 
     @Override
     public void onVideoLoaded(Video item) {
+        updateHistory(item);
+    }
+
+    private void updateHistory(Video item) {
         MediaService service = YouTubeMediaService.instance();
         MediaItemManager mediaItemManager = service.getMediaItemManager();
 
-        mHistoryAction = mediaItemManager.updateHistoryPositionObserve(item.mediaItem, mController.getPositionMs() / 1_000)
+        Observable<Void> historyObservable;
+
+        long positionSec = mController.getPositionMs() / 1_000;
+
+        if (item.mediaItem != null) {
+            historyObservable = mediaItemManager.updateHistoryPositionObserve(item.mediaItem, positionSec);
+        } else { // video launched form ATV channels
+            historyObservable = mediaItemManager.updateHistoryPositionObserve(item.videoId, positionSec);
+        }
+
+        mHistoryAction = historyObservable
                 .subscribeOn(Schedulers.newThread())
                 .subscribe((Void v) -> {}, error -> Log.e(TAG, "History update error: " + error));
     }
