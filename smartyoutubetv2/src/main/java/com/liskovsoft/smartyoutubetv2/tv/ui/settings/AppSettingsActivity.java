e43eaf3f3365375c0083f8f78523c7e0b0214569
Yuriy Liskov <firsthash@gmail.com>
2020-09-26 20:00:06 +0300

    Android 8 second fix

--- smartyoutubetv2/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/settings/AppSettingsActivity.java
+++ smartyoutubetv2/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/settings/AppSettingsActivity.java
@@ -1,6 +1,7 @@
 package com.liskovsoft.smartyoutubetv2.tv.ui.settings;
 
 import android.content.pm.ActivityInfo;
+import android.os.Build.VERSION;
 import android.os.Bundle;
 import androidx.fragment.app.FragmentActivity;
 import com.liskovsoft.smartyoutubetv2.tv.R;
@@ -11,7 +12,6 @@ public class AppSettingsActivity extends FragmentActivity {
         setupActivity();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_video_settings);
-
     }
 
     private void setupActivity() {
@@ -19,6 +19,8 @@ public class AppSettingsActivity extends FragmentActivity {
         // Error happen only on Android O (api 26) when you set "portrait" orientation in manifest
         // So, to fix the problem, set orientation here instead of manifest
         // More info: https://stackoverflow.com/questions/48072438/java-lang-illegalstateexception-only-fullscreen-opaque-activities-can-request-o
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        if (VERSION.SDK_INT != 26) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
     }
 }
