156be29ae23b67caf647ce358acace8b8193db3e
Yuriy Liskov <firsthash@gmail.com>
2020-10-13 11:29:08 +0300

    preview: animated text

--- smartyoutubetv2/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/widgets/textbadgecard/TextBadgeImageCardView.java
+++ smartyoutubetv2/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/widgets/textbadgecard/TextBadgeImageCardView.java
@@ -1,6 +1,7 @@
 package com.liskovsoft.smartyoutubetv2.tv.ui.widgets.textbadgecard;
 
 import android.content.Context;
+import android.text.TextUtils.TruncateAt;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,6 +31,28 @@ public class TextBadgeImageCardView extends ImageCardView {
         createTextBadge();
     }
 
+    private void enableTextAnimation(boolean enable) {
+        TextView title = findViewById(R.id.title_text);
+        TextView content = findViewById(R.id.content_text);
+
+        if (title == null || content == null) {
+            return;
+        }
+
+        if (enable) {
+            title.setEllipsize(TruncateAt.MARQUEE);
+            title.setMarqueeRepeatLimit(-1);
+            title.setHorizontallyScrolling(true);
+
+            content.setEllipsize(TruncateAt.MARQUEE);
+            content.setMarqueeRepeatLimit(-1);
+            content.setHorizontallyScrolling(true);
+        } else {
+            title.setEllipsize(TruncateAt.END);
+            content.setEllipsize(TruncateAt.END);
+        }
+    }
+
     private void createTextBadge() {
         ViewGroup wrapper = findViewById(R.id.main_image_wrapper);
 
@@ -55,5 +78,12 @@ public class TextBadgeImageCardView extends ImageCardView {
         } else {
             mBadgeText.setVisibility(View.GONE);
         }
+    }
+
+    @Override
+    public void setSelected(boolean selected) {
+        super.setSelected(selected);
+
+        enableTextAnimation(selected);
     }
 }
