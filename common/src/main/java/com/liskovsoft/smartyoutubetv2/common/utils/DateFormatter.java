848752a077ad121dea91959964011ca164616bfd
Yuriy Liskov <firsthash@gmail.com>
2020-10-29 22:42:28 +0200

    date format fix

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/utils/DateFormatter.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/utils/DateFormatter.java
@@ -1,7 +1,6 @@
 package com.liskovsoft.smartyoutubetv2.common.utils;
-;
+
 import android.content.Context;
-import android.text.format.DateFormat;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -10,11 +9,20 @@ import java.util.Locale;
 public class DateFormatter {
     public static String getCurrentDateShort(Context context) {
         // details: https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
-        String pattern = DateFormat.is24HourFormat(context) ? "EEE d MMM H:mm" : "EEE d MMM h:mm a";
+        String pattern = is24HourLocale() ? "EEE d MMM H:mm" : "EEE d MMM h:mm a";
 
         SimpleDateFormat serverFormat = new SimpleDateFormat(pattern, Locale.getDefault());
         String currentTime = serverFormat.format(new Date());
 
         return String.format("%1$s", currentTime);
+    }
+
+    private static boolean is24HourLocale() {
+        String output = SimpleDateFormat.getTimeInstance().format(new Date());
+        if (output.contains(" AM") || output.contains(" PM")) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
