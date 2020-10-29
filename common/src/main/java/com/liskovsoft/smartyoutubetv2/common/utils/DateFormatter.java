bc5bf028f12bf12d81cede95c0a829da80440a0b
Yuriy Liskov <firsthash@gmail.com>
2020-10-29 22:50:49 +0200

    date format upd

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/utils/DateFormatter.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/utils/DateFormatter.java
@@ -1,6 +1,7 @@
 package com.liskovsoft.smartyoutubetv2.common.utils;
 
 import android.content.Context;
+import com.liskovsoft.sharedutils.locale.LocaleUtility;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -9,7 +10,7 @@ import java.util.Locale;
 public class DateFormatter {
     public static String getCurrentDateShort(Context context) {
         // details: https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
-        String pattern = is24HourLocale() ? "EEE d MMM H:mm" : "EEE d MMM h:mm a";
+        String pattern = is24HourLocale(context) ? "EEE d MMM H:mm" : "EEE d MMM h:mm a";
 
         SimpleDateFormat serverFormat = new SimpleDateFormat(pattern, Locale.getDefault());
         String currentTime = serverFormat.format(new Date());
@@ -17,10 +18,21 @@ public class DateFormatter {
         return String.format("%1$s", currentTime);
     }
 
-    private static boolean is24HourLocale() {
-        String output = SimpleDateFormat.getTimeInstance().format(new Date());
-        if (output.contains(" AM") || output.contains(" PM")) {
-            return false;
+    private static boolean is24HourLocale(Context context) {
+        Locale locale = LocaleUtility.getCurrentLocale(context);
+
+        java.text.DateFormat natural =
+                java.text.DateFormat.getTimeInstance(
+                        java.text.DateFormat.LONG, locale);
+
+        if (natural instanceof SimpleDateFormat) {
+            SimpleDateFormat sdf = (SimpleDateFormat) natural;
+            String pattern = sdf.toPattern();
+            if (pattern.indexOf('H') >= 0) {
+                return true;
+            } else {
+                return false;
+            }
         } else {
             return true;
         }
