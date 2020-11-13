98b986b9175d9ba2d53d51388aacc9ee8ac1905e
Yuriy Liskov <firsthash@gmail.com>
2020-11-13 02:08:47 +0200

    add Kivi to render override

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/other/V2/CustomOverridesRenderersFactory.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/other/V2/CustomOverridesRenderersFactory.java
@@ -25,7 +25,7 @@ import java.util.ArrayList;
  */
 public class CustomOverridesRenderersFactory extends DefaultRenderersFactory {
     private static final String TAG = CustomOverridesRenderersFactory.class.getSimpleName();
-    private static final String[] FRAME_DROP_FIX_LIST = {"T95ZPLUS (q201_3GB)", "UGOOS (UGOOS)"};
+    private static final String[] FRAME_DROP_FIX_LIST = {"T95ZPLUS (q201_3GB)", "UGOOS (UGOOS)", "55UC30G (ctl_iptv_mrvl)"};
 
     public CustomOverridesRenderersFactory(FragmentActivity activity) {
         super(activity);
