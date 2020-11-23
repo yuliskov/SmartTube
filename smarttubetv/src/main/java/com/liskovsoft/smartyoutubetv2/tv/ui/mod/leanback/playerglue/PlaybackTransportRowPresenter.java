1e772272edabb8e54bf83132c7c343e1f1461f9e
vadim <vadimdolgachev@gmail.com>
2020-11-23 22:36:28 +0700

    set min time interval for calling  onSeekPositionChanged

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/mod/leanback/playerglue/PlaybackTransportRowPresenter.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/mod/leanback/playerglue/PlaybackTransportRowPresenter.java
@@ -75,6 +75,7 @@ public class PlaybackTransportRowPresenter extends PlaybackRowPresenter {
         private static final long SPEED_INCREASE_PERIOD_MS = 1000;
         private static final double SPEED_INCREASE_FACTOR = 1.5;
         private static final long START_SEEK_INCREMENT_MS = 10_000;
+        private static final long MIN_INTERVAL_CLIENT_SEEK_TO_POSITION_MS = 500;
         final Presenter.ViewHolder mDescriptionViewHolder;
         final ImageView mImageView;
         final ViewGroup mDescriptionDock;
@@ -110,6 +111,7 @@ public class PlaybackTransportRowPresenter extends PlaybackRowPresenter {
         int mPositionsLength;
         long mSeekIncrementMs = -1;
         long mSeekStartTimeMs;
+        long mLastTimeSeekPosition = 0;
 
         // MOD: update quality info
         final QualityInfoListener mQualityListener = this::setQualityInfo;
@@ -195,7 +197,11 @@ public class PlaybackTransportRowPresenter extends PlaybackRowPresenter {
             }
             double ratio = (double) newPos / mTotalTimeInMs;     // Range: [0, 1]
             mProgressBar.setProgress((int) (ratio * Integer.MAX_VALUE)); // Could safely cast to int
-            mSeekClient.onSeekPositionChanged(newPos);
+
+            if ((System.currentTimeMillis() - mLastTimeSeekPosition) > MIN_INTERVAL_CLIENT_SEEK_TO_POSITION_MS) {
+                mSeekClient.onSeekPositionChanged(newPos);
+                mLastTimeSeekPosition = System.currentTimeMillis();
+            }
         }
 
         void resetSeekIncrement() {
@@ -209,7 +215,7 @@ public class PlaybackTransportRowPresenter extends PlaybackRowPresenter {
         long calculateSeekIncrement() {
             if (mSeekIncrementMs == -1) {
                 mSeekStartTimeMs = System.currentTimeMillis();
-                mSeekIncrementMs = START_SEEK_INCREMENT_MS;
+                mSeekIncrementMs = Math.max(mTotalTimeInMs / 550, START_SEEK_INCREMENT_MS);
             } else {
                 // increase seek speed by 1.5 every 1 second
                 long timePassed = System.currentTimeMillis() - mSeekStartTimeMs;
