7dedb000d1af151192432c59727b0a7a4ec0e584
vadim <vadimdolgachev@gmail.com>
2020-11-25 20:26:51 +0700

    add remain flavors files

---
+++ common/src/stbeta/java/com/liskovsoft/smartyoutubetv2/common/app/FlavorConfig.java
@@ -0,0 +1,12 @@
+package com.liskovsoft.smartyoutubetv2.common.app;
+
+public class FlavorConfig {
+    public static class Player {
+        public final static int MAX_HEIGHT_VIDEO_RESOLUTION = Integer.MAX_VALUE;
+    }
+
+    public static class AppPrefs {
+        public final static float VideoGridScale = 1.0f;
+        public final static int ColorSchemeIndex = 0;
+    }
+}
