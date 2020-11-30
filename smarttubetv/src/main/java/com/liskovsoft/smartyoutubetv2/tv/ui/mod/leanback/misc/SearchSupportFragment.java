6fad1df87ac591e1a3ec7101e5005fab6618cc3f
vadim <vadimdolgachev@gmail.com>
2020-11-30 14:47:50 +0700

    fix focus behaviour in search screen

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/mod/leanback/misc/SearchSupportFragment.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/mod/leanback/misc/SearchSupportFragment.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.text.Editable;
+import android.text.Selection;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -223,6 +226,7 @@ public class SearchSupportFragment extends Fragment {
     private ExternalQuery mExternalQuery;
 
     private SpeechRecognizer mSpeechRecognizer;
+    private boolean mScrollToEndAfterTextChanged;
 
     int mStatus;
     boolean mAutoStartRecognition = false; // MOD: don't start search immediately
@@ -338,6 +342,10 @@ public class SearchSupportFragment extends Fragment {
         mSearchTextEditor.setOnFocusChangeListener((v, focused) -> {
             Log.d(TAG, "on search field focused");
             if (focused && mRowsSupportFragment != null && mRowsSupportFragment.getVerticalGridView() != null) {
+                // scroll cursor to end after transition from lb_search_bar_speech_orb
+                mScrollToEndAfterTextChanged = true;
+                Selection.setSelection(mSearchTextEditor.getText(), mSearchTextEditor.length());
+
                 mRowsSupportFragment.getVerticalGridView().clearFocus();
 
                 if (getContext() != null) {
@@ -347,7 +355,26 @@ public class SearchSupportFragment extends Fragment {
                 }
             }
         });
-        
+
+        mSearchTextEditor.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (mScrollToEndAfterTextChanged) {
+                    Selection.setSelection(mSearchTextEditor.getText(), mSearchTextEditor.length());
+                    mScrollToEndAfterTextChanged = false;
+                }
+            }
+        });
         //mSearchTextEditor.setOnClickListener(v -> {
         //    Log.d(TAG, "on search field clicked");
         //
@@ -370,6 +397,7 @@ public class SearchSupportFragment extends Fragment {
                 }
             }
         });
+        mSpeechOrbView.setClickable(false);
         // End MOD
 
         readArguments(getArguments());
@@ -836,5 +864,9 @@ public class SearchSupportFragment extends Fragment {
             mQuery = query;
             mSubmit = submit;
         }
+    }
+
+    protected int getSearchTextEditorId() {
+        return mSearchTextEditor != null ? mSearchTextEditor.getId() : 0;
     }
 }
