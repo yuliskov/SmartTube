16fc1810d3a58467414bf87b326b49ff74ab328c
Yuriy Liskov <firsthash@gmail.com>
2020-12-12 12:57:34 +0200

    more ui scale options

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/settings/MainUISettingsPresenter.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/settings/MainUISettingsPresenter.java
@@ -81,7 +81,7 @@ public class MainUISettingsPresenter extends BasePresenter<Void> {
     private void appendScaleUI(AppSettingsPresenter settingsPresenter) {
         List<OptionItem> options = new ArrayList<>();
 
-        for (float scale : new float[] {0.6f, 0.65f, 0.7f, 0.75f, 0.8f, 0.85f, 0.9f, 0.95f, 1.0f}) {
+        for (float scale : new float[] {0.6f, 0.65f, 0.7f, 0.75f, 0.8f, 0.85f, 0.9f, 0.95f, 1.0f, 1.05f, 1.1f, 1.15f, 1.2f}) {
             options.add(UiOptionItem.from(String.format("%sx", scale),
                     optionItem -> {
                         mMainUIData.setUIScale(scale);
