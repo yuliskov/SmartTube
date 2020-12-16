3afbfc89a0f05adb83599bfff9965e56961cd163
Yuriy Liskov <firsthash@gmail.com>
2020-12-16 08:07:52 +0200

    discord push test

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/settings/PlayerSettingsPresenter.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/settings/PlayerSettingsPresenter.java
@@ -10,17 +10,21 @@ import com.liskovsoft.smartyoutubetv2.common.app.models.playback.ui.OptionItem;
 import com.liskovsoft.smartyoutubetv2.common.app.models.playback.ui.UiOptionItem;
 import com.liskovsoft.smartyoutubetv2.common.app.presenters.AppSettingsPresenter;
 import com.liskovsoft.smartyoutubetv2.common.app.presenters.base.BasePresenter;
+import com.liskovsoft.smartyoutubetv2.common.autoframerate.FormatItem;
+import com.liskovsoft.smartyoutubetv2.common.misc.LangUpdater;
 import com.liskovsoft.smartyoutubetv2.common.prefs.PlayerData;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 public class PlayerSettingsPresenter extends BasePresenter<Void> {
-    private final PlayerData mPlayerUIData;
+    private final PlayerData mPlayerData;
 
     public PlayerSettingsPresenter(Context context) {
         super(context);
-        mPlayerUIData = PlayerData.instance(context);
+        mPlayerData = PlayerData.instance(context);
     }
 
     public static PlayerSettingsPresenter instance(Context context) {
@@ -35,6 +39,7 @@ public class PlayerSettingsPresenter extends BasePresenter<Void> {
         appendVideoPresetsCategory(settingsPresenter);
         appendBackgroundPlaybackCategory(settingsPresenter);
         appendAutoFrameRateCategory(settingsPresenter);
+        appendSubtitleLanguageCategory(settingsPresenter);
         appendSubtitleStyleCategory(settingsPresenter);
         appendOKButtonCategory(settingsPresenter);
         appendUIAutoHideCategory(settingsPresenter);
@@ -48,18 +53,18 @@ public class PlayerSettingsPresenter extends BasePresenter<Void> {
 
         options.add(UiOptionItem.from(
                 getContext().getString(R.string.player_only_ui),
-                option -> mPlayerUIData.setOKButtonBehavior(PlayerData.ONLY_UI),
-                mPlayerUIData.getOKButtonBehavior() == PlayerData.ONLY_UI));
+                option -> mPlayerData.setOKButtonBehavior(PlayerData.ONLY_UI),
+                mPlayerData.getOKButtonBehavior() == PlayerData.ONLY_UI));
 
         options.add(UiOptionItem.from(
                 getContext().getString(R.string.player_ui_and_pause),
-                option -> mPlayerUIData.setOKButtonBehavior(PlayerData.UI_AND_PAUSE),
-                mPlayerUIData.getOKButtonBehavior() == PlayerData.UI_AND_PAUSE));
+                option -> mPlayerData.setOKButtonBehavior(PlayerData.UI_AND_PAUSE),
+                mPlayerData.getOKButtonBehavior() == PlayerData.UI_AND_PAUSE));
 
         options.add(UiOptionItem.from(
                 getContext().getString(R.string.player_only_pause),
-                option -> mPlayerUIData.setOKButtonBehavior(PlayerData.ONLY_PAUSE),
-                mPlayerUIData.getOKButtonBehavior() == PlayerData.ONLY_PAUSE));
+                option -> mPlayerData.setOKButtonBehavior(PlayerData.ONLY_PAUSE),
+                mPlayerData.getOKButtonBehavior() == PlayerData.ONLY_PAUSE));
 
         settingsPresenter.appendRadioCategory(getContext().getString(R.string.player_ok_button_behavior), options);
     }
@@ -69,42 +74,65 @@ public class PlayerSettingsPresenter extends BasePresenter<Void> {
 
         options.add(UiOptionItem.from(
                 getContext().getString(R.string.player_ui_hide_never),
-                option -> mPlayerUIData.setUIHideTimoutSec(PlayerData.AUTO_HIDE_NEVER),
-                mPlayerUIData.getUIHideTimoutSec() == PlayerData.AUTO_HIDE_NEVER));
+                option -> mPlayerData.setUIHideTimoutSec(PlayerData.AUTO_HIDE_NEVER),
+                mPlayerData.getUIHideTimoutSec() == PlayerData.AUTO_HIDE_NEVER));
 
         for (int i = 1; i <= 15; i++) {
             int timeoutSec = i;
             options.add(UiOptionItem.from(
                     String.format("%s sec", i),
-                    option -> mPlayerUIData.setUIHideTimoutSec(timeoutSec),
-                    mPlayerUIData.getUIHideTimoutSec() == i));
+                    option -> mPlayerData.setUIHideTimoutSec(timeoutSec),
+                    mPlayerData.getUIHideTimoutSec() == i));
         }
 
         settingsPresenter.appendRadioCategory(getContext().getString(R.string.player_ui_hide_behavior), options);
     }
 
     private void appendVideoBufferCategory(AppSettingsPresenter settingsPresenter) {
-        OptionCategory category = HqDialogManager.createVideoBufferCategory(getContext(), mPlayerUIData);
+        OptionCategory category = HqDialogManager.createVideoBufferCategory(getContext(), mPlayerData);
         settingsPresenter.appendRadioCategory(category.title, category.options);
     }
 
     private void appendVideoPresetsCategory(AppSettingsPresenter settingsPresenter) {
-        OptionCategory category = HqDialogManager.createVideoPresetsCategory(getContext(), mPlayerUIData);
+        OptionCategory category = HqDialogManager.createVideoPresetsCategory(getContext(), mPlayerData);
         settingsPresenter.appendRadioCategory(category.title, category.options);
     }
 
     private void appendBackgroundPlaybackCategory(AppSettingsPresenter settingsPresenter) {
-        OptionCategory category = HqDialogManager.createBackgroundPlaybackCategory(getContext(), mPlayerUIData);
+        OptionCategory category = HqDialogManager.createBackgroundPlaybackCategory(getContext(), mPlayerData);
         settingsPresenter.appendRadioCategory(category.title, category.options);
     }
 
     private void appendAutoFrameRateCategory(AppSettingsPresenter settingsPresenter) {
-        OptionCategory category = AutoFrameRateManager.createAutoFrameRateCategory(getContext(), mPlayerUIData);
+        OptionCategory category = AutoFrameRateManager.createAutoFrameRateCategory(getContext(), mPlayerData);
         settingsPresenter.appendCheckedCategory(category.title, category.options);
     }
 
+    private void appendSubtitleLanguageCategory(AppSettingsPresenter settingsPresenter) {
+        String subtitleLanguageTitle = getContext().getString(R.string.subtitle_language);
+        String subtitlesDisabled = getContext().getString(R.string.subtitles_disabled);
+
+        LangUpdater langUpdater = new LangUpdater(getContext());
+        HashMap<String, String> locales = langUpdater.getSupportedLocales();
+        FormatItem currentFormat = mPlayerData.getFormat(FormatItem.TYPE_SUBTITLE);
+
+        List<OptionItem> options = new ArrayList<>();
+
+        options.add(UiOptionItem.from(
+                subtitlesDisabled, option -> mPlayerData.setFormat(FormatItem.fromLanguage(null)),
+                currentFormat == null || currentFormat.equals(FormatItem.fromLanguage(null))));
+
+        for (Entry<String, String> entry : locales.entrySet()) {
+            options.add(UiOptionItem.from(
+                    entry.getKey(), option -> mPlayerData.setFormat(FormatItem.fromLanguage(entry.getValue())),
+                    FormatItem.fromLanguage(entry.getValue()).equals(currentFormat)));
+        }
+        
+        settingsPresenter.appendRadioCategory(subtitleLanguageTitle, options);
+    }
+
     private void appendSubtitleStyleCategory(AppSettingsPresenter settingsPresenter) {
-        OptionCategory category = PlayerUiManager.createSubtitleStylesCategory(getContext(), mPlayerUIData);
+        OptionCategory category = PlayerUiManager.createSubtitleStylesCategory(getContext(), mPlayerData);
         settingsPresenter.appendRadioCategory(category.title, category.options);
     }
 
@@ -112,24 +140,24 @@ public class PlayerSettingsPresenter extends BasePresenter<Void> {
         List<OptionItem> options = new ArrayList<>();
 
         options.add(UiOptionItem.from(getContext().getString(R.string.player_full_date),
-                option -> mPlayerUIData.showFullDate(option.isSelected()),
-                mPlayerUIData.isShowFullDateEnabled()));
+                option -> mPlayerData.showFullDate(option.isSelected()),
+                mPlayerData.isShowFullDateEnabled()));
 
         options.add(UiOptionItem.from(getContext().getString(R.string.player_seek_preview),
-                option -> mPlayerUIData.enableSeekPreview(option.isSelected()),
-                mPlayerUIData.isSeekPreviewEnabled()));
+                option -> mPlayerData.enableSeekPreview(option.isSelected()),
+                mPlayerData.isSeekPreviewEnabled()));
 
         options.add(UiOptionItem.from(getContext().getString(R.string.player_pause_when_seek),
-                option -> mPlayerUIData.enablePauseOnSeek(option.isSelected()),
-                mPlayerUIData.isPauseOnSeekEnabled()));
+                option -> mPlayerData.enablePauseOnSeek(option.isSelected()),
+                mPlayerData.isPauseOnSeekEnabled()));
 
         options.add(UiOptionItem.from(getContext().getString(R.string.player_show_clock),
-                option -> mPlayerUIData.enableClock(option.isSelected()),
-                mPlayerUIData.isClockEnabled()));
+                option -> mPlayerData.enableClock(option.isSelected()),
+                mPlayerData.isClockEnabled()));
 
         options.add(UiOptionItem.from(getContext().getString(R.string.player_show_remaining_time),
-                option -> mPlayerUIData.enableRemainingTime(option.isSelected()),
-                mPlayerUIData.isRemainingTimeEnabled()));
+                option -> mPlayerData.enableRemainingTime(option.isSelected()),
+                mPlayerData.isRemainingTimeEnabled()));
 
         settingsPresenter.appendCheckedCategory(getContext().getString(R.string.player_other), options);
     }
