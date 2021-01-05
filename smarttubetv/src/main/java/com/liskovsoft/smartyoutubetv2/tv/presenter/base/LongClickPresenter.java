1378cdae0a3ea7b2c2b8379e30ab522f6997b393
Yuriy Liskov <firsthash@gmail.com>
2021-01-05 16:00:21 +0200

    long click refactor

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/presenter/base/LongClickPresenter.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/presenter/base/LongClickPresenter.java
@@ -1,6 +1,8 @@
 package com.liskovsoft.smartyoutubetv2.tv.presenter.base;
 
+import android.view.KeyEvent;
 import androidx.leanback.widget.Presenter;
+import com.liskovsoft.sharedutils.helpers.KeyHelpers;
 
 public abstract class LongClickPresenter extends Presenter {
     private OnItemViewLongClickedListener mListener;
@@ -16,8 +18,17 @@ public abstract class LongClickPresenter extends Presenter {
                 if (mListener != null) {
                     mListener.onItemLongClicked(viewHolder, item);
                 }
-                return true;
+                return true; // don't provoke single click event
             });
+
+            //viewHolder.view.setOnKeyListener((v, keyCode, event) -> {
+            //    if (event.getAction() == KeyEvent.ACTION_DOWN && KeyHelpers.isMenuKey(keyCode)) {
+            //        if (mListener != null) {
+            //            mListener.onItemLongClicked(viewHolder, item);
+            //        }
+            //    }
+            //    return false; // enable navigation events
+            //});
         }
     }
 
@@ -25,6 +36,7 @@ public abstract class LongClickPresenter extends Presenter {
     public void onUnbindViewHolder(ViewHolder viewHolder) {
         if (mListener != null) {
             viewHolder.view.setOnLongClickListener(null);
+            //viewHolder.view.setOnKeyListener(null);
         }
     }
 }
