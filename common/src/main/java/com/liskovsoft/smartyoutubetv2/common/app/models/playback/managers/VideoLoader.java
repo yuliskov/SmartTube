902296b62b45b216e79fadbc6f42e871edea8760
Yuriy Liskov <firsthash@gmail.com>
2021-02-13 00:31:33 +0200

    refactor

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/managers/VideoLoader.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/managers/VideoLoader.java
@@ -73,7 +73,8 @@ public class VideoLoader extends PlayerEventListenerHelper {
     public void onEngineError(int type) {
         Log.e(TAG, "Player error occurred: %s. Trying to fix…", type);
 
-        if (type == PlayerEventListener.ERROR_TYPE_SOURCE) {
+        if (type == PlayerEventListener.ERROR_TYPE_SOURCE ||
+            type == PlayerEventListener.ERROR_TYPE_RENDERER) {
             // Some ciphered data might be stalled.
             // Might happen when the app wasn't used quite a long time.
             YouTubeMediaService.instance().invalidateCache();
