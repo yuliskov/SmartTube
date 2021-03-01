e41a8cb04856cace74eb88e5ba1468355c434a46
Yuriy Liskov <firsthash@gmail.com>
2021-03-01 07:20:44 +0200

    refactor

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/RemoteControlService.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/RemoteControlService.java
@@ -1,21 +1,25 @@
 package com.liskovsoft.smartyoutubetv2.common.misc;
 
-import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
+import android.os.IBinder;
 import androidx.annotation.Nullable;
 import com.liskovsoft.sharedutils.helpers.Helpers;
 import com.liskovsoft.sharedutils.mylogger.Log;
 
-public class RemoteControlService extends IntentService {
+public class RemoteControlService extends Service {
     private static final String TAG = RemoteControlService.class.getSimpleName();
 
-    public RemoteControlService() {
-        super(RemoteControlService.class.getSimpleName());
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
     @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
-        // NOP
-        Log.d(TAG, "onHandleIntent: " + Helpers.toString(intent));
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: %s", Helpers.toString(intent));
+
+        return super.onStartCommand(intent, flags, startId);
     }
 }
