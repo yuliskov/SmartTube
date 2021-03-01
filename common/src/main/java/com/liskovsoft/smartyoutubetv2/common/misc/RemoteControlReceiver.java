ed1b93aa7f238a12058a7565eb612b0916915380
Yuriy Liskov <firsthash@gmail.com>
2021-03-01 08:29:29 +0200

    refactor

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/RemoteControlReceiver.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/RemoteControlReceiver.java
@@ -3,8 +3,14 @@ package com.liskovsoft.smartyoutubetv2.common.misc;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 import com.liskovsoft.sharedutils.mylogger.Log;
 import com.liskovsoft.smartyoutubetv2.common.app.presenters.PlaybackPresenter;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class RemoteControlReceiver extends BroadcastReceiver {
     private static final String TAG = RemoteControlReceiver.class.getSimpleName();
@@ -13,10 +19,27 @@ public class RemoteControlReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "Initializing remote control listener...");
 
+        startService(context);
+
+        startWorkRequest(context);
+
+        PlaybackPresenter.instance(context); // init RemoteControlListener
+    }
+
+    private void startService(Context context) {
         // Fake service to prevent the app from destroying
         Intent serviceIntent = new Intent(context, RemoteControlService.class);
         context.startService(serviceIntent);
+    }
 
-        PlaybackPresenter.instance(context); // init RemoteControlListener
+    private void startWorkRequest(Context context) {
+        WorkRequest workRequest =
+                new PeriodicWorkRequest.Builder(
+                        RemoteControlWorker.class, 30, TimeUnit.MINUTES
+                ).build();
+
+        WorkManager
+                .getInstance(context)
+                .enqueue(workRequest);
     }
 }
