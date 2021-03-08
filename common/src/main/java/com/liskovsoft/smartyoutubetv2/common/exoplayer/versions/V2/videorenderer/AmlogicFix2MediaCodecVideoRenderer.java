98c8cfcde3274d3179181f81eaa5e3e807cabc01
Yuriy Liskov <firsthash@gmail.com>
2021-03-08 08:56:37 +0200

    refactor

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/versions/V2/videorenderer/AmlogicFix2MediaCodecVideoRenderer.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/versions/V2/videorenderer/AmlogicFix2MediaCodecVideoRenderer.java
@@ -31,7 +31,8 @@ public class AmlogicFix2MediaCodecVideoRenderer extends MediaCodecVideoRenderer 
 
         sVideoCodecName = codecInfo.name;
 
-        if ("OMX.amlogic.avc.decoder.awesome".equals(codecInfo.name)
+        if (("OMX.amlogic.avc.decoder.awesome".equals(codecInfo.name) ||
+             "OMX.amlogic.vp9.decoder.awesome".equals(codecInfo.name))
                 && Util.SDK_INT <= 25
                 && (maxValues.width < 1920 || maxValues.height < 1089)) {
             return new CodecMaxValues(
