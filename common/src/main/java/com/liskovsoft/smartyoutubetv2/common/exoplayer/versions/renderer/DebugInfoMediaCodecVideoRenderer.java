b2ade643453b8dd3f2c6e8937eb5e631c7c74b7e
Yuriy Liskov <firsthash@gmail.com>
2021-03-30 19:08:54 +0300

    refactor

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/versions/renderer/DebugInfoMediaCodecVideoRenderer.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/versions/renderer/DebugInfoMediaCodecVideoRenderer.java
@@ -1,6 +1,7 @@
 package com.liskovsoft.smartyoutubetv2.common.exoplayer.versions.renderer;
 
 import android.content.Context;
+import android.media.MediaCodec;
 import android.os.Handler;
 import androidx.annotation.Nullable;
 import com.google.android.exoplayer2.Format;
@@ -10,10 +11,12 @@ import com.google.android.exoplayer2.mediacodec.MediaCodecInfo;
 import com.google.android.exoplayer2.mediacodec.MediaCodecSelector;
 import com.google.android.exoplayer2.video.MediaCodecVideoRenderer;
 import com.google.android.exoplayer2.video.VideoRendererEventListener;
+import com.liskovsoft.sharedutils.mylogger.Log;
 import com.liskovsoft.smartyoutubetv2.common.exoplayer.versions.ExoUtils;
 
 public class DebugInfoMediaCodecVideoRenderer extends MediaCodecVideoRenderer {
     private static final String TAG = DebugInfoMediaCodecVideoRenderer.class.getSimpleName();
+    //private long mPrevPresentationTimeUs;
 
     // Exo 2.9
     //public DebugInfoMediaCodecVideoRenderer(Context context, MediaCodecSelector mediaCodecSelector, long allowedJoiningTimeMs,
@@ -44,4 +47,21 @@ public class DebugInfoMediaCodecVideoRenderer extends MediaCodecVideoRenderer {
 
         return super.getCodecMaxValues(codecInfo, format, streamFormats);
     }
+
+    // Measure real fps.
+    // 29.97fps test: https://www.youtube.com/watch?v=LXb3EKWsInQ (Costa Rica)
+    // More info: https://github.com/google/ExoPlayer/issues/4088
+    //@Override
+    //protected void renderOutputBuffer(MediaCodec codec, int index, long presentationTimeUs) {
+    //    super.renderOutputBuffer(codec, index, presentationTimeUs);
+    //}
+    //
+    //@Override
+    //protected void renderOutputBufferV21(MediaCodec codec, int index, long presentationTimeUs, long releaseTimeNs) {
+    //    super.renderOutputBufferV21(codec, index, presentationTimeUs, releaseTimeNs);
+    //
+    //    Log.d(TAG, "Real fps: %s", 1_000_000f / (presentationTimeUs - mPrevPresentationTimeUs));
+    //
+    //    mPrevPresentationTimeUs = presentationTimeUs;
+    //}
 }
