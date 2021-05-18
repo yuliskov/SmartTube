315dd3ba175d528553b06fb398a45cdff4d10a40
Yuriy Liskov <firsthash@gmail.com>
2021-05-18 20:29:10 +0300

    bugfix

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/AppSettingsPresenter.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/AppSettingsPresenter.java
@@ -144,7 +144,8 @@ public class AppSettingsPresenter extends BasePresenter<AppSettingsView> {
 
     public boolean isDialogShown() {
         // Also check that current dialog almost closed (new view start is pending from a menu item)
-        return !mCategories.isEmpty() && !ViewManager.instance(getContext()).isNewViewPending();
+        // Hmm. Maybe current dialog is pending. Check that view is null.
+        return !mCategories.isEmpty() && (!ViewManager.instance(getContext()).isNewViewPending() || getView() == null);
     }
 
     public void appendRadioCategory(String categoryTitle, List<OptionItem> items) {
