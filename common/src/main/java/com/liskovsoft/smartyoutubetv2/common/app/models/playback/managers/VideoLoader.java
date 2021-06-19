81662a744404ecff91d9a44ff95556e7020379f1
Yuriy Liskov <firsthash@gmail.com>
2021-06-19 04:39:36 +0300

    bump to 11.40

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/managers/VideoLoader.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/managers/VideoLoader.java
@@ -278,7 +278,7 @@ public class VideoLoader extends PlayerEventListenerHelper {
     private void processFormatInfo(MediaItemFormatInfo formatInfo) {
         if (formatInfo.isUnplayable()) {
             getController().showError(formatInfo.getPlayabilityStatus());
-        } else if (formatInfo.containsDashUrl() && formatInfo.isLive()) {
+        } else if (formatInfo.containsDashUrl()) {
             Log.d(TAG, "Found live video in dash format. Loading...");
             getController().openDashUrl(formatInfo.getDashManifestUrl());
         } else if (formatInfo.containsHlsUrl()) {
