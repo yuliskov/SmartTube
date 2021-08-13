c6d451c070025c0c08ddee261d21b691746e04de
Yuriy Liskov <firsthash@gmail.com>
2021-08-13 15:06:32 +0300

    exo release fix

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/controller/ExoPlayerController.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/controller/ExoPlayerController.java
@@ -158,7 +158,7 @@ public class ExoPlayerController implements Player.EventListener, PlayerControll
 
         if (mPlayer != null) {
             mPlayer.removeListener(this);
-            //mPlayer.stop();
+            mPlayer.stop();
             mPlayer.release();
             mPlayer = null;
         }
