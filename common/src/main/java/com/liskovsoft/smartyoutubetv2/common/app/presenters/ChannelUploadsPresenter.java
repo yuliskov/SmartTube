50f6a1666605735b6654e756953d284c7bd1c729
Yuriy Liskov <firsthash@gmail.com>
2021-08-30 05:56:10 +0300

    pinned items observe fix

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/ChannelUploadsPresenter.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/ChannelUploadsPresenter.java
@@ -144,7 +144,10 @@ public class ChannelUploadsPresenter extends BasePresenter<ChannelUploadsView> i
         return item.hasUploads() ?
                 mGroupManager.getGroupObserve(item.mediaItem) :
                 mItemManager.getMetadataObserve(item.videoId, item.playlistId, 0)
-                        .flatMap(mediaItemMetadata -> Observable.just(findPlaylistRow(mediaItemMetadata)));
+                        .flatMap(mediaItemMetadata -> {
+                            MediaGroup playlistRow = findPlaylistRow(mediaItemMetadata);
+                            return playlistRow != null ? Observable.just(playlistRow) : Observable.empty();
+                        });
     }
 
     private void updateGrid(Video item) {
