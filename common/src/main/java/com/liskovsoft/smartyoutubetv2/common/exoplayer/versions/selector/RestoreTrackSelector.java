b5b072bca1e89a160853af41d6ccc87d3d8bd7e1
Yuriy Liskov <firsthash@gmail.com>
2021-09-18 01:56:59 +0300

    revert player settings

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/versions/selector/RestoreTrackSelector.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/versions/selector/RestoreTrackSelector.java
@@ -30,7 +30,7 @@ public class RestoreTrackSelector extends DefaultTrackSelector {
     public RestoreTrackSelector(Factory trackSelectionFactory) {
         super(trackSelectionFactory);
         // Could help with Shield resolution bug?
-        setParameters(buildUponParameters().setForceHighestSupportedBitrate(true));
+        //setParameters(buildUponParameters().setForceHighestSupportedBitrate(true));
     }
 
     public void setOnTrackSelectCallback(TrackSelectorCallback callback) {
