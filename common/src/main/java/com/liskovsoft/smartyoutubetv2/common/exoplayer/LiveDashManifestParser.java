92452f888054af26183c86cbc7651166b1390051
Yuriy Liskov <firsthash@gmail.com>
2021-12-04 23:10:58 +0200

    revert changes

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/LiveDashManifestParser.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/LiveDashManifestParser.java
@@ -83,7 +83,7 @@ public class LiveDashManifestParser extends DashManifestParser {
         //long oldSegmentNum = getFirstSegmentNum(mManifest);
 
         // Last segments sometimes may produce 404 error (see DrLupo streams).
-        newSegmentNum -= 3;
+        //newSegmentNum -= 5;
 
         Period oldPeriod = mOldManifest.getPeriod(0);
         Period newPeriod = newManifest.getPeriod(0);
