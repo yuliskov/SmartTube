a447b8dd8566bda1c06499adabd7e626311bf164
Yuriy Liskov <firsthash@gmail.com>
2021-12-06 00:49:03 +0200

    add track error fixer: upd

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/errors/TrackErrorFixer.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/errors/TrackErrorFixer.java
@@ -7,7 +7,9 @@ import com.liskovsoft.smartyoutubetv2.common.exoplayer.selector.track.MediaTrack
 import java.util.Set;
 
 public class TrackErrorFixer {
+    private static final long BLACKLIST_INTERVAL_MS = 10_000;
     private final TrackSelectorManager mTrackSelectorManager;
+    private long mSelectionTimeMs;
 
     public TrackErrorFixer(TrackSelectorManager trackSelectorManager) {
         mTrackSelectorManager = trackSelectorManager;
@@ -15,6 +17,10 @@ public class TrackErrorFixer {
 
     public boolean fixError(ExoPlaybackException error) {
         if (error.type != ExoPlaybackException.TYPE_SOURCE) {
+            return false;
+        }
+
+        if (System.currentTimeMillis() - mSelectionTimeMs < BLACKLIST_INTERVAL_MS) {
             return false;
         }
 
@@ -30,6 +36,7 @@ public class TrackErrorFixer {
 
         if (tmpTrack != null) {
             mTrackSelectorManager.selectTrack(tmpTrack);
+            mSelectionTimeMs = System.currentTimeMillis();
             return true;
         }
 
