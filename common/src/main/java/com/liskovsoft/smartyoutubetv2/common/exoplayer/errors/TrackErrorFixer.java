8b25ac8f1e929eb3588b525a1a650ead5834b717
Yuriy Liskov <firsthash@gmail.com>
2021-12-06 20:34:32 +0200

    add track error fixer: upd

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/errors/TrackErrorFixer.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/errors/TrackErrorFixer.java
@@ -1,18 +1,28 @@
 package com.liskovsoft.smartyoutubetv2.common.exoplayer.errors;
 
+import android.os.Handler;
 import com.google.android.exoplayer2.upstream.HttpDataSource.InvalidResponseCodeException;
 import com.liskovsoft.smartyoutubetv2.common.exoplayer.selector.TrackSelectorManager;
 import com.liskovsoft.smartyoutubetv2.common.exoplayer.selector.track.MediaTrack;
+import com.liskovsoft.smartyoutubetv2.common.utils.Utils;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class TrackErrorFixer {
-    private static final long BLACKLIST_INTERVAL_MS = 3_000;
+    private static final int BLACKLIST_CHECK_INTERVAL_MS = 1_000;
+    private static final int BLACKLIST_CLEAR_INTERVAL_MS = 5_000;
+    private static final int SELECT_FIRST_TRACK_INTERVAL_MS = 30_000;
     private final TrackSelectorManager mTrackSelectorManager;
+    private final Handler mHandler;
     private long mSelectionTimeMs;
+    private final Set<MediaTrack> mBlacklistedTracks = new HashSet<>();
+    private InvalidResponseCodeException mLastEx;
+    private final Runnable mSelectFirstTrack = this::selectFirstTrack;
 
     public TrackErrorFixer(TrackSelectorManager trackSelectorManager) {
         mTrackSelectorManager = trackSelectorManager;
+        mHandler = new Handler();
     }
 
     /**
@@ -25,30 +35,80 @@ public class TrackErrorFixer {
             return false;
         }
 
-        if (((InvalidResponseCodeException) e).responseCode != 404) {
+        InvalidResponseCodeException ex = (InvalidResponseCodeException) e;
+
+        if (ex.responseCode != 404) {
             return false;
         }
 
-        if (System.currentTimeMillis() - mSelectionTimeMs < BLACKLIST_INTERVAL_MS) {
+        if (System.currentTimeMillis() - mSelectionTimeMs < BLACKLIST_CHECK_INTERVAL_MS) {
             return false;
         }
 
-        Set<MediaTrack> audioTracks = mTrackSelectorManager.getAudioTracks();
+        mLastEx = ex;
+
+        return selectNextTrack();
+    }
+
+    private boolean selectNextTrack() {
+        if (mLastEx == null) {
+            return false;
+        }
+
+        if (System.currentTimeMillis() - mSelectionTimeMs > BLACKLIST_CLEAR_INTERVAL_MS) {
+            mBlacklistedTracks.clear();
+        }
+
+        Set<MediaTrack> tracks = isAudio(mLastEx) ? mTrackSelectorManager.getAudioTracks() : mTrackSelectorManager.getVideoTracks();
         MediaTrack tmpTrack = null;
 
-        for (MediaTrack track : audioTracks) {
-            if (!track.isSelected) {
+        for (MediaTrack track : tracks) {
+            if (track.isSelected) {
+                mBlacklistedTracks.add(track);
+            } else {
                 tmpTrack = track;
-                break;
+                if (!mBlacklistedTracks.contains(track)) {
+                    mBlacklistedTracks.add(track);
+                    break;
+                }
             }
         }
 
         if (tmpTrack != null) {
             mTrackSelectorManager.selectTrack(tmpTrack);
             mSelectionTimeMs = System.currentTimeMillis();
+            Utils.postDelayed(mHandler, mSelectFirstTrack, SELECT_FIRST_TRACK_INTERVAL_MS);
             return true;
         }
 
         return false;
+    }
+
+    private void selectFirstTrack() {
+        if (mLastEx == null) {
+            return;
+        }
+
+        mBlacklistedTracks.clear();
+
+        Set<MediaTrack> tracks = isAudio(mLastEx) ? mTrackSelectorManager.getAudioTracks() : mTrackSelectorManager.getVideoTracks();
+        MediaTrack tmpTrack = null;
+
+        for (MediaTrack track : tracks) {
+            mBlacklistedTracks.add(track);
+            tmpTrack = track;
+            break;
+        }
+
+        if (tmpTrack != null) {
+            mTrackSelectorManager.selectTrack(tmpTrack);
+            mSelectionTimeMs = System.currentTimeMillis();
+        }
+    }
+
+    private boolean isAudio(InvalidResponseCodeException ex) {
+        String url = ex.dataSpec.uri.toString();
+
+        return url.contains("mime/audio");
     }
 }
