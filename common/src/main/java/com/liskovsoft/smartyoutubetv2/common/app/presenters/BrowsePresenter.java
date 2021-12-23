867c2c47875a1652f8b5c5b13c1e655efc1b7306
Yuriy Liskov <firsthash@gmail.com>
2021-12-23 23:44:29 +0200

    typo

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/BrowsePresenter.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/BrowsePresenter.java
@@ -319,7 +319,7 @@ public class BrowsePresenter extends BasePresenter<BrowseView> implements Sectio
 
         if (item.belongsToChannelUploads()) { // We need to be sure we exactly on Channels section
             ChannelUploadsMenuPresenter.instance(getContext()).showMenu(item, (videoItem, action) -> {
-                if (action == VideoMenuCallback.ACTION_UNSUBSCRIBE) {
+                if (action == VideoMenuCallback.ACTION_UNSUBSCRIBE) { // works with any uploads section look
                     VideoGroup removedGroup = VideoGroup.from(item);
                     removedGroup.setAction(VideoGroup.ACTION_REMOVE);
                     getView().updateSection(removedGroup);
