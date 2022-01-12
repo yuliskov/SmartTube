2beb7c78e57398395296c5aa240082d83eb46636
Yuriy Liskov <firsthash@gmail.com>
2022-01-12 15:16:35 +0200

    key remapping fix

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/PlayerKeyTranslator.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/PlayerKeyTranslator.java
@@ -22,6 +22,11 @@ public class PlayerKeyTranslator extends GlobalKeyTranslator {
     private void initKeyMapping() {
         Map<Integer, Integer> globalKeyMapping = getKeyMapping();
 
+        // Reset global mapping to default
+        globalKeyMapping.remove(KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
+        globalKeyMapping.remove(KeyEvent.KEYCODE_MEDIA_REWIND);
+        globalKeyMapping.remove(KeyEvent.KEYCODE_MEDIA_FAST_FORWARD);
+
         if (mGeneralData.isRemapFastForwardToNextEnabled()) {
             globalKeyMapping.put(KeyEvent.KEYCODE_MEDIA_FAST_FORWARD, KeyEvent.KEYCODE_MEDIA_NEXT);
             globalKeyMapping.put(KeyEvent.KEYCODE_MEDIA_REWIND, KeyEvent.KEYCODE_MEDIA_PREVIOUS);
@@ -36,11 +41,6 @@ public class PlayerKeyTranslator extends GlobalKeyTranslator {
             globalKeyMapping.put(KeyEvent.KEYCODE_PAGE_UP, KeyEvent.KEYCODE_UNKNOWN);
             globalKeyMapping.put(KeyEvent.KEYCODE_PAGE_DOWN, KeyEvent.KEYCODE_UNKNOWN);
         }
-
-        // Reset global mapping to default
-        globalKeyMapping.remove(KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
-        globalKeyMapping.remove(KeyEvent.KEYCODE_MEDIA_REWIND);
-        globalKeyMapping.remove(KeyEvent.KEYCODE_MEDIA_FAST_FORWARD);
     }
 
     private void initActionMapping() {
