896907cbf99c7de8357b9eb7e2b20b33257bb6b8
Yuriy Liskov <firsthash@gmail.com>
2022-01-22 15:39:30 +0200

    typo

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/AppDialogPresenter.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/AppDialogPresenter.java
@@ -154,6 +154,7 @@ public class AppDialogPresenter extends BasePresenter<AppDialogView> {
     public boolean isDialogShown() {
         // Also check that current dialog almost closed (new view start is pending from a menu item)
         // Hmm. Maybe current dialog is pending. Check that view is null.
+        // Also check that we aren't started the same view (nested dialog).
         return !mCategories.isEmpty() && (!ViewManager.instance(getContext()).isNewViewPending(AppDialogView.class) || getView() == null);
     }
 
