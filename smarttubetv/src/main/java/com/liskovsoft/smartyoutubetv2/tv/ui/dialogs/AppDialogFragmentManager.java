98f6decbd545fa015f7ec6a16a201571b08c93db
Yuriy Liskov <firsthash@gmail.com>
2022-03-17 03:55:37 +0200

    refactor

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/dialogs/AppDialogFragmentManager.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/dialogs/AppDialogFragmentManager.java
@@ -65,6 +65,9 @@ public class AppDialogFragmentManager {
 
         initMultiSelectListPreference(category, pref);
 
+        //pref.setDialogLayoutResource(R.layout.text_dialog);
+        //pref.setDialogMessage(category.items.get(0).getDescription());
+
         return pref;
     }
 
