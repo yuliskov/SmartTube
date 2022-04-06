94ec1db8fb03950060922f0f27ad9fa1d1960b12
Yuriy Liskov <firsthash@gmail.com>
2022-04-06 22:36:34 +0300

    revert changes

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/views/ViewManager.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/views/ViewManager.java
@@ -12,7 +12,6 @@ import com.liskovsoft.sharedutils.helpers.FileHelpers;
 import com.liskovsoft.sharedutils.helpers.MessageHelpers;
 import com.liskovsoft.sharedutils.locale.LocaleUpdater;
 import com.liskovsoft.sharedutils.mylogger.Log;
-import com.liskovsoft.sharedutils.rx.RxUtils;
 import com.liskovsoft.smartyoutubetv2.common.app.presenters.BrowsePresenter;
 import com.liskovsoft.smartyoutubetv2.common.app.presenters.PlaybackPresenter;
 import com.liskovsoft.smartyoutubetv2.common.app.presenters.SplashPresenter;
@@ -335,12 +334,6 @@ public class ViewManager {
             mActivityStack.clear();
 
             ((MotherActivity) activity).finishReally();
-
-            // Fix: Can't open the app from history after finish
-            RxUtils.runAsync(() -> {
-                mIsMoveToBackEnabled = false;
-                mIsFinishing = false;
-            }, 1_000);
         }
     }
 
