8c8bce06bd206241afe67cccdef46e1d47b4ac70
Yuriy Liskov <firsthash@gmail.com>
2022-04-16 23:33:00 +0300

    typo

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/MotherActivity.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/MotherActivity.java
@@ -70,7 +70,7 @@ public class MotherActivity extends FragmentActivity {
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
-            boolean isScreenOffAndSeeking = mScreensaverManager.isScreenOff() && Helpers.equalsAny((int) event.getKeyCode(), KeyEvent.KEYCODE_DPAD_LEFT, KeyEvent.KEYCODE_DPAD_RIGHT);
+            boolean isScreenOffAndSeeking = mScreensaverManager.isScreenOff() && Helpers.equalsAny(event.getKeyCode(), new int[]{KeyEvent.KEYCODE_DPAD_LEFT, KeyEvent.KEYCODE_DPAD_RIGHT});
             if (!isScreenOffAndSeeking) {
                 mScreensaverManager.enable();
             }
