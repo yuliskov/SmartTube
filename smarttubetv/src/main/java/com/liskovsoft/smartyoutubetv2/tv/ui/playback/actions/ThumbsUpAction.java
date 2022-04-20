0cf522a054c3323939f0b2557d4a157637b40033
Yuriy Liskov <firsthash@gmail.com>
2022-04-20 08:05:28 +0300

    add player tooltips: upd labels

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/playback/actions/ThumbsUpAction.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/playback/actions/ThumbsUpAction.java
@@ -6,5 +6,11 @@ import com.liskovsoft.smartyoutubetv2.tv.R;
 public class ThumbsUpAction extends ThumbsAction {
     public ThumbsUpAction(Context context) {
         super(context, R.id.action_thumbs_up, R.drawable.lb_ic_thumb_up);
+
+        String[] labels = new String[2];
+        // Note, labels denote the action taken when clicked
+        labels[INDEX_OFF] = context.getString(R.string.action_like);
+        labels[INDEX_ON] = context.getString(R.string.action_like);
+        setLabels(labels);
     }
 }
