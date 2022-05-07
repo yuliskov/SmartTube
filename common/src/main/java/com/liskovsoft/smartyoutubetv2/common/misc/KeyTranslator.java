3922f812b94b62c9e91abc68c30b201de9677ff6
Yuriy Liskov <firsthash@gmail.com>
2022-05-07 13:09:35 +0300

    unpaired enter button fix

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/KeyTranslator.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/KeyTranslator.java
@@ -1,6 +1,5 @@
 package com.liskovsoft.smartyoutubetv2.common.misc;
 
-import android.app.Instrumentation;
 import android.view.KeyEvent;
 import com.liskovsoft.sharedutils.mylogger.Log;
 import com.liskovsoft.sharedutils.rx.RxUtils;
@@ -10,6 +9,7 @@ import java.util.Map;
 
 public abstract class KeyTranslator {
     private static final String TAG = KeyTranslator.class.getSimpleName();
+    private KeyEvent mHandledEvent;
 
     public final boolean translate(KeyEvent event) {
         Map<Integer, Integer> keyMapping = getKeyMapping();
@@ -36,6 +36,8 @@ public abstract class KeyTranslator {
 
         if (newKeyEvent != event) {
             handled = true;
+
+            checkAndFixUnpairedEvent(newKeyEvent);
 
             RxUtils.runAsync(() -> Utils.sendKey(newKeyEvent));
         }
@@ -64,6 +66,32 @@ public abstract class KeyTranslator {
         Log.d(TAG, "Translating %s to %s", origin, newKey);
 
         return newKey;
+    }
+
+    private void checkAndFixUnpairedEvent(KeyEvent newKeyEvent) {
+        if (newKeyEvent == null) {
+            return;
+        }
+
+        if (newKeyEvent.getAction() == KeyEvent.ACTION_UP &&
+                (mHandledEvent == null || mHandledEvent.getKeyCode() != newKeyEvent.getKeyCode())) {
+            KeyEvent keyDownEvent = new KeyEvent(
+                    newKeyEvent.getDownTime(),
+                    newKeyEvent.getEventTime(),
+                    KeyEvent.ACTION_DOWN,
+                    newKeyEvent.getKeyCode(),
+                    newKeyEvent.getRepeatCount(),
+                    newKeyEvent.getMetaState(),
+                    newKeyEvent.getDeviceId(),
+                    newKeyEvent.getScanCode(),
+                    newKeyEvent.getFlags(),
+                    newKeyEvent.getSource()
+            );
+
+            RxUtils.runAsync(() -> Utils.sendKey(keyDownEvent));
+        }
+
+        mHandledEvent = newKeyEvent;
     }
 
     protected abstract Map<Integer, Integer> getKeyMapping();
