7510d4e572b06c08776bebaf85c286b5e19dc638
Yuriy Liskov <firsthash@gmail.com>
2022-06-12 01:51:28 +0300

    add webview activity: first step: upd

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/webview/WebViewActivity.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/webview/WebViewActivity.java
@@ -1,7 +1,6 @@
 package com.liskovsoft.smartyoutubetv2.tv.ui.webview;
 
 import android.os.Bundle;
-import com.liskovsoft.smartyoutubetv2.common.prefs.MainUIData;
 import com.liskovsoft.smartyoutubetv2.tv.R;
 import com.liskovsoft.smartyoutubetv2.tv.ui.common.LeanbackActivity;
 
@@ -12,13 +11,5 @@ public class WebViewActivity extends LeanbackActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_webview);
-    }
-
-    @Override
-    protected void initTheme() {
-        int browseThemeResId = MainUIData.instance(this).getColorScheme().browseThemeResId;
-        if (browseThemeResId > 0) {
-            setTheme(browseThemeResId);
-        }
     }
 }
