5051a83fd04fc64092297dddf1ef74d4fe8e3932
Yuriy Liskov <firsthash@gmail.com>
2022-06-11 06:52:32 +0300

    player: streams: add black placeholder

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/managers/VideoLoaderManager.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/managers/VideoLoaderManager.java
@@ -32,6 +32,7 @@ import java.util.Map;
 
 public class VideoLoaderManager extends PlayerEventListenerHelper {
     private static final String TAG = VideoLoaderManager.class.getSimpleName();
+    private static final String BLACK_PLACEHOLDER_URL = "https://via.placeholder.com/1280x720/000000/000000";
     private final Playlist mPlaylist;
     private final Handler mHandler;
     private final SuggestionsLoaderManager mSuggestionsLoader;
@@ -336,7 +337,7 @@ public class VideoLoaderManager extends PlayerEventListenerHelper {
             Log.d(TAG, "Empty format info received. Seems future live translation. No video data to pass to the player.");
             scheduleReloadVideoTimer(30 * 1_000);
             mSuggestionsLoader.loadSuggestions(mLastVideo);
-            bgImageUrl = mLastVideo.bgImageUrl;
+            bgImageUrl = mLastVideo.bgImageUrl != null ? mLastVideo.bgImageUrl : BLACK_PLACEHOLDER_URL;
         }
 
         Video video = getController().getVideo();
