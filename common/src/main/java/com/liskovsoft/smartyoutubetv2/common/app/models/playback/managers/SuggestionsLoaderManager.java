8c597685a6a79c691805278817bf25d86ebc98f4
Yuriy Liskov <firsthash@gmail.com>
2022-06-14 22:08:02 +0300

    typo

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/managers/SuggestionsLoaderManager.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/managers/SuggestionsLoaderManager.java
@@ -186,6 +186,7 @@ public class SuggestionsLoaderManager extends PlayerEventListenerHelper {
                         error -> {
                             Log.e(TAG, "loadSuggestions error: %s", error.getMessage());
                             error.printStackTrace();
+                            // normally error could happen (cause unknown)
                         }
                 );
     }
