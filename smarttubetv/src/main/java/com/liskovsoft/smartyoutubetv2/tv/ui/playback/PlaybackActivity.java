65003893e771992abd648cab72c08b3e8430f4cc
Yuriy Liskov <firsthash@gmail.com>
2022-07-05 19:26:20 +0300

    typo

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/playback/PlaybackActivity.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/playback/PlaybackActivity.java
@@ -251,6 +251,7 @@ public class PlaybackActivity extends LeanbackActivity {
     @Override
     public void onUserLeaveHint() {
         // Check that user not open dialog/search activity instead of really leaving the activity
+        // Activity may be overlapped by the dialog, back is pressed or new view started
         if (!AppDialogPresenter.instance(this).isDialogShown() && !mBackPressed && !mViewManager.isNewViewPending()) {
             switch (mPlaybackFragment.getBackgroundMode()) {
                 case PlaybackEngineController.BACKGROUND_MODE_PLAY_BEHIND:
