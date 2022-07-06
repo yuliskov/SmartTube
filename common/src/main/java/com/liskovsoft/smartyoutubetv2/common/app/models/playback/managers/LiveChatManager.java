75f1edaf44be9d7b01f6a65ab2366d69c250b417
Yuriy Liskov <firsthash@gmail.com>
2022-07-07 02:20:33 +0300

    live chat: upd

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/managers/LiveChatManager.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/managers/LiveChatManager.java
@@ -4,8 +4,11 @@ import com.liskovsoft.mediaserviceinterfaces.LiveChatService;
 import com.liskovsoft.mediaserviceinterfaces.data.MediaItemMetadata;
 import com.liskovsoft.sharedutils.mylogger.Log;
 import com.liskovsoft.sharedutils.rx.RxUtils;
+import com.liskovsoft.smartyoutubetv2.common.app.models.data.Video;
 import com.liskovsoft.smartyoutubetv2.common.app.models.playback.PlayerEventListenerHelper;
 import com.liskovsoft.smartyoutubetv2.common.app.models.playback.managers.SuggestionsLoaderManager.MetadataListener;
+import com.liskovsoft.smartyoutubetv2.common.app.models.playback.ui.UiOptionItem;
+import com.liskovsoft.smartyoutubetv2.common.app.presenters.AppDialogPresenter;
 import com.liskovsoft.youtubeapi.service.YouTubeMediaService;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
@@ -23,18 +26,28 @@ public class LiveChatManager extends PlayerEventListenerHelper implements Metada
     @Override
     public void onMetadata(MediaItemMetadata metadata) {
         if (metadata != null && metadata.getLiveChatKey() != null) {
-            openLiveChat(metadata.getLiveChatKey());
+            //openLiveChat(metadata.getLiveChatKey());
         }
     }
 
     private void openLiveChat(String chatKey) {
         disposeActions();
 
+        AppDialogPresenter dialogPresenter = AppDialogPresenter.instance(getActivity());
+        dialogPresenter.clear();
+        Video video = getController().getVideo();
+        String title = String.format("%s - %s", video.getTitle(), video.getAuthor());
+        //dialogPresenter.appendChatCategory(title, UiOptionItem.from(description, null));
+        dialogPresenter.showDialog(title);
+
         mChatAction = mChatService.openLiveChatObserve(chatKey)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                        chatItem -> Log.d(TAG, chatItem.getMessage()),
+                        chatItem -> {
+                            Log.d(TAG, chatItem.getMessage());
+                            //optionItem.onChatItem(chatItem);
+                        },
                         error -> {
                             Log.e(TAG, error.getMessage());
                             error.printStackTrace();
@@ -54,6 +67,10 @@ public class LiveChatManager extends PlayerEventListenerHelper implements Metada
     }
 
     private void disposeActions() {
-        RxUtils.disposeActions(mChatAction);
+        if (RxUtils.isAnyActionRunning(mChatAction)) {
+            RxUtils.disposeActions(mChatAction);
+            AppDialogPresenter dialogPresenter = AppDialogPresenter.instance(getActivity());
+            dialogPresenter.closeDialog();
+        }
     }
 }
