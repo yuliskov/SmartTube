04cfe63f351602f9339f0638512065cd25d4f2bb
Yuriy Liskov <firsthash@gmail.com>
2022-07-21 05:14:36 +0300

    upd

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/settings/PlayerSettingsPresenter.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/settings/PlayerSettingsPresenter.java
@@ -298,13 +298,14 @@ public class PlayerSettingsPresenter extends BasePresenter<Void> {
                 option -> GlobalPreferences.instance(getContext()).enableChannelsService(!option.isSelected()),
                 !GlobalPreferences.instance(getContext()).isChannelsServiceEnabled()));
 
-        options.add(UiOptionItem.from("Prefer IPv4 DNS",
-                option -> GlobalPreferences.instance(getContext()).preferIPv4Dns(option.isSelected()),
-                GlobalPreferences.instance(getContext()).isIPv4DnsPreferred()));
-
-        options.add(UiOptionItem.from("Enable DNS over HTTPS",
-                option -> GlobalPreferences.instance(getContext()).enableDnsOverHttps(option.isSelected()),
-                GlobalPreferences.instance(getContext()).isDnsOverHttpsEnabled()));
+        // Disabled inside RetrofitHelper
+        //options.add(UiOptionItem.from("Prefer IPv4 DNS",
+        //        option -> GlobalPreferences.instance(getContext()).preferIPv4Dns(option.isSelected()),
+        //        GlobalPreferences.instance(getContext()).isIPv4DnsPreferred()));
+        //
+        //options.add(UiOptionItem.from("Enable DNS over HTTPS",
+        //        option -> GlobalPreferences.instance(getContext()).enableDnsOverHttps(option.isSelected()),
+        //        GlobalPreferences.instance(getContext()).isDnsOverHttpsEnabled()));
 
         // Need to be enabled on older version of ExoPlayer (e.g. 2.10.6).
         // It's because there's no tweaks for modern devices.
