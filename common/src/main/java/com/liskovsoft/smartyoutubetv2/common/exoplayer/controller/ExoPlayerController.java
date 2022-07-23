4f9cb4af4491d4fa7734e348e8b1ff0178392e22
Yuriy Liskov <firsthash@gmail.com>
2022-07-23 20:06:21 +0300

    typo

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/controller/ExoPlayerController.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/controller/ExoPlayerController.java
@@ -178,7 +178,7 @@ public class ExoPlayerController implements Player.EventListener, PlayerControll
 
         if (mPlayer != null) {
             mPlayer.removeListener(this);
-            //mPlayer.stop();
+            //mPlayer.stop(true);
             mPlayer.release();
             mPlayer = null;
         }
