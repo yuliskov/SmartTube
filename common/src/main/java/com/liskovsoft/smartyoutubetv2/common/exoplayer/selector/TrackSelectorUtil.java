7d2e7bcf8a1f527d778faf2d2850aa4857345a2e
vadim <vadimdolgachev@gmail.com>
2022-08-12 10:21:48 +0700

    fix merge

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/selector/TrackSelectorUtil.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/selector/TrackSelectorUtil.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 
 public class TrackSelectorUtil {
     public static final String CODEC_SHORT_AV1 = "av01";
+    private static final String CODEC_SHORT_AV1_FIXED = "av1";
     private static final String CODEC_SHORT_AVC = "avc";
     public static final String CODEC_SHORT_VP9 = "vp9";
     private static final String CODEC_SHORT_VP9_HDR = "vp9.2";
