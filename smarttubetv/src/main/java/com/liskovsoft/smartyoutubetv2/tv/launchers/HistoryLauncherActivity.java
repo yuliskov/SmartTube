fb9dc902670bd2aa1f21d648d1ebc2f985090613
Yuriy Liskov <firsthash@gmail.com>
2023-03-29 18:14:55 +0300

    remove old tests + shortcut launchers refactor

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/launchers/HistoryLauncherActivity.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/launchers/HistoryLauncherActivity.java
@@ -1,36 +1,17 @@
 package com.liskovsoft.smartyoutubetv2.tv.launchers;
 
-import android.content.Intent;
 import android.os.Bundle;
 import com.liskovsoft.mediaserviceinterfaces.data.MediaGroup;
 import com.liskovsoft.smartyoutubetv2.common.app.presenters.BrowsePresenter;
-import com.liskovsoft.smartyoutubetv2.common.app.views.SplashView;
 import com.liskovsoft.smartyoutubetv2.common.misc.MotherActivity;
 
-public class HistoryLauncherActivity extends MotherActivity implements SplashView {
-    private static final String TAG = HistoryLauncherActivity.class.getSimpleName();
-    private Intent mNewIntent;
-
+public class HistoryLauncherActivity extends MotherActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mNewIntent = getIntent();
-
         BrowsePresenter.instance(this).selectSection(MediaGroup.TYPE_HISTORY);
 
         finish();
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-
-        mNewIntent = intent;
-    }
-
-    @Override
-    public Intent getNewIntent() {
-        return mNewIntent;
     }
 }
