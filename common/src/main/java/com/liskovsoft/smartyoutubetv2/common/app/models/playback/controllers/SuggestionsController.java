782539fd367ecdbb6a8972495d5e837ba04c947e
Yuriy Liskov <firsthash@gmail.com>
2023-05-28 04:23:19 +0300

    player: queue fix

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/controllers/SuggestionsController.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/controllers/SuggestionsController.java
@@ -434,9 +434,6 @@ public class SuggestionsController extends PlayerEventListenerHelper {
         VideoGroup videoGroup = VideoGroup.from(queue);
         videoGroup.setTitle(getActivity().getString(R.string.action_playback_queue));
         videoGroup.setId(videoGroup.getTitle().hashCode());
-        for (Video item : queue) {
-            item.setGroup(videoGroup);
-        }
         getPlayer().updateSuggestions(videoGroup);
     }
 
