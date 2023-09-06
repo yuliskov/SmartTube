dff712c75bbfe7aa1994680daba492bbbd8cbaaf
Yuriy Liskov <firsthash@gmail.com>
2023-09-06 13:31:49 +0300

    refactor

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/adapter/VideoGroupObjectAdapter.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/adapter/VideoGroupObjectAdapter.java
@@ -26,27 +26,10 @@ public class VideoGroupObjectAdapter extends ObjectAdapter {
         //        // Dirty hack for avoiding group duplication.
         //        // Duplicated items suddenly appeared in Home and Subscriptions.
         //
-        //        //if (size() >= c.size() && c.contains(get(c.size() - 1))) {
-        //        //    return false;
-        //        //}
-        //
-        //        // Alt method. Works with Home rows.
-        //        //if (size() < 30) {
-        //        //    Video firstItem = Helpers.get(c, 0);
-        //        //    if (firstItem != null && contains(firstItem)) {
-        //        //        return false;
-        //        //    }
-        //        //}
-        //
         //        // Another alt method.
         //        if (size() > 0 && size() < 30) {
         //            Helpers.removeIf(c, this::contains);
         //        }
-        //
-        //        // Latest alt dubs fix method (not works).
-        //        //Set<Video> uniqueItems = new LinkedHashSet<>(c);
-        //
-        //        //return super.addAll(uniqueItems);
         //
         //        return super.addAll(c);
         //    }
