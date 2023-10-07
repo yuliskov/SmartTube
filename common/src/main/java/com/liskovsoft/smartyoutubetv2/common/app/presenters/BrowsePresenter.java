ff2ee17313b4dd5b014b62a7f5aa65c6e79687c3
Yuriy Liskov <firsthash@gmail.com>
2023-10-07 05:31:23 +0300

    bump to 19.44

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/BrowsePresenter.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/BrowsePresenter.java
@@ -990,6 +990,6 @@ public class BrowsePresenter extends BasePresenter<BrowseView> implements Sectio
 
     @Override
     public void onProfileChanged() {
-        
+        Log.d(TAG, "On profile changed");
     }
 }
