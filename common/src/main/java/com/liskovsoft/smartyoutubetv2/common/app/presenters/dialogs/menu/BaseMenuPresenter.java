6337ccbdde1209f158a7c1b4a67aeb7f65b8432c
Yuriy Liskov <firsthash@gmail.com>
2023-10-30 00:44:09 +0200

    browse: upd pin msg

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/dialogs/menu/BaseMenuPresenter.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/dialogs/menu/BaseMenuPresenter.java
@@ -122,7 +122,7 @@ public abstract class BaseMenuPresenter extends BasePresenter<Void> {
         } else {
             presenter.pinItem(section);
         }
-        MessageHelpers.showMessage(getContext(), getContext().getString(isItemPinned ? R.string.unpinned_from_sidebar : R.string.pinned_to_sidebar) + "\n" + section.title);
+        MessageHelpers.showMessage(getContext(), section.title + "\n" + getContext().getString(isItemPinned ? R.string.unpinned_from_sidebar : R.string.pinned_to_sidebar));
     }
 
     private Video createPinnedPlaylist(Video video) {
