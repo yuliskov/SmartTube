9173db2cad170269c60190aa56be91187ac0b7dd
Yuriy Liskov <firsthash@gmail.com>
2023-11-16 20:53:31 +0200

    Android 4 qr code fix

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/signin/SignInFragment.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/signin/SignInFragment.java
@@ -1,5 +1,6 @@
 package com.liskovsoft.smartyoutubetv2.tv.ui.signin;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import androidx.annotation.NonNull;
@@ -9,6 +10,11 @@ import androidx.leanback.app.GuidedStepSupportFragment;
 import androidx.leanback.widget.GuidanceStylist;
 import androidx.leanback.widget.GuidedAction;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.liskovsoft.sharedutils.mylogger.Log;
 import com.liskovsoft.smartyoutubetv2.common.app.presenters.SignInPresenter;
 import com.liskovsoft.smartyoutubetv2.common.app.views.SignInView;
 import com.liskovsoft.smartyoutubetv2.common.utils.Utils;
@@ -18,6 +24,7 @@ import com.liskovsoft.smartyoutubetv2.tv.util.ViewUtil;
 import java.util.List;
 
 public class SignInFragment extends GuidedStepSupportFragment implements SignInView {
+    private static final String TAG = SignInFragment.class.getSimpleName();
     private static final int CONTINUE = 2;
     private static final String SIGN_IN_URL_SHORT = "https://yt.be/activate"; // doesn't support query params
     private static final String SIGN_IN_URL_FULL = "https://youtube.com/tv/activate"; // support query params
@@ -58,7 +65,8 @@ public class SignInFragment extends GuidedStepSupportFragment implements SignInV
 
         Glide.with(getContext())
                 .load(Utils.toQrCodeLink(SIGN_IN_URL_FULL + "?user_code=" + userCode.replace(" ", "-")))
-                .apply(ViewUtil.glideOptions())
+                .apply(ViewUtil.glideOptions()).error(ContextCompat.getDrawable(getContext(), R.drawable.activate_account_qrcode))
+                .listener(mErrorListener)
                 .into(getGuidanceStylist().getIconView());
     }
 
@@ -90,4 +98,17 @@ public class SignInFragment extends GuidedStepSupportFragment implements SignInV
             mSignInPresenter.onActionClicked();
         }
     }
+
+    private final RequestListener<Drawable> mErrorListener = new RequestListener<Drawable>() {
+        @Override
+        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+            Log.e(TAG, "Glide load failed: " + e);
+            return false;
+        }
+
+        @Override
+        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+            return false;
+        }
+    };
 }
