358bc4cd2481c6259def81f0d046724eef87650c
Yuriy Liskov <firsthash@gmail.com>
2024-07-27 14:00:49 +0300

    player: error fixer upd

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/controllers/VideoLoaderController.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/controllers/VideoLoaderController.java
@@ -501,7 +501,8 @@ public class VideoLoaderController extends PlayerEventListenerHelper implements 
                 mPlayerData.setVideoBufferType(PlayerData.BUFFER_LOW);
             }
         } else if (Helpers.startsWithAny(error.getMessage(),
-                "Unable to connect to", "Invalid NAL length", "Response code: 421", "Invalid integer size")) {
+                "Unable to connect to", "Invalid NAL length", "Response code: 421",
+                "Invalid integer size", "Unexpected ArrayIndexOutOfBoundsException")) {
             // Switch between network engines in hope that one of them fixes the error
             //mPlayerTweaksData.setPlayerDataSource(getNextEngine());
             YouTubeServiceManager.instance().applyNoPlaybackFix();
