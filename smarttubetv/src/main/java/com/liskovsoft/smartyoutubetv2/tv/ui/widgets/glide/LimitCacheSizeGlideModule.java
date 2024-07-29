eb5f2e5125a17e388e94242c33b0dcfc3cc7aa60
Yuriy Liskov <firsthash@gmail.com>
2024-07-30 01:09:42 +0300

    glide: cache size upd

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/widgets/glide/LimitCacheSizeGlideModule.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/widgets/glide/LimitCacheSizeGlideModule.java
@@ -11,10 +11,12 @@ import com.bumptech.glide.module.AppGlideModule;
  * https://stackoverflow.com/questions/46108915/how-to-increase-the-cache-size-in-glide-android
  */
 public class LimitCacheSizeGlideModule extends AppGlideModule {
+    private final static long CACHE_SIZE = 1024 * 1024 * 10; // 10 MB
+
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
         //if (MyApplication.from(context).isTest())
         //    return; // NOTE: StatFs will crash on robolectric.
-        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, 1024 * 1024 * 20)); // 20 MB
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, CACHE_SIZE));
     }
 }
