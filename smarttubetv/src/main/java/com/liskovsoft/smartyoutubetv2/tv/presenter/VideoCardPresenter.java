a3b935f69d05c0cdb0231da163e0288986d9c222
Yuriy Liskov <firsthash@gmail.com>
2024-07-30 00:56:13 +0300

    glide thumbs set cache size to 20mb

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/presenter/VideoCardPresenter.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/presenter/VideoCardPresenter.java
@@ -13,6 +13,7 @@ import androidx.core.content.ContextCompat;
 import androidx.leanback.widget.Presenter;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
@@ -142,10 +143,10 @@ public class VideoCardPresenter extends LongClickPresenter {
                 //.asBitmap() // disable animation (webp, gif)
                 .load(ClickbaitRemover.updateThumbnail(video, mThumbQuality))
                 //.placeholder(mDefaultCardImage)
-                .apply(ViewUtil.glideOptions()
-                    // improve image compression on low end devices
-                    .override(mWidth, mHeight)
-                )
+                .apply(ViewUtil.glideOptions())
+                // improve image compression on low end devices
+                .override(mWidth, mHeight)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .listener(mErrorListener)
                 .error(
                     // Updated thumbnail url not found
