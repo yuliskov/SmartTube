e92ac5ff9963bacaf14ed0638819971f027199bd
Yuriy Liskov <firsthash@gmail.com>
2024-08-30 03:13:31 +0300

    player: none buffer size upd

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/other/ExoPlayerInitializer.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/exoplayer/other/ExoPlayerInitializer.java
@@ -120,8 +120,8 @@ public class ExoPlayerInitializer {
             case PlayerData.BUFFER_NONE:
                 minBufferMs = 5_000; // LIVE fix
                 maxBufferMs = 5_000; // LIVE fix
-                bufferForPlaybackMs = 1_000;
-                bufferForPlaybackAfterRebufferMs = 1_000;
+                //bufferForPlaybackMs = 1_000;
+                //bufferForPlaybackAfterRebufferMs = 1_000;
                 break;
         }
 
