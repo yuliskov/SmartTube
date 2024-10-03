dd84b8b437c665ae189f0a6a50382ce2a8dceb85
Yuriy Liskov <firsthash@gmail.com>
2024-10-04 02:09:10 +0300

    channel groups id generator fix

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/dialogs/menu/providers/channelgroup/ChannelGroup.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/presenters/dialogs/menu/providers/channelgroup/ChannelGroup.java
@@ -55,7 +55,7 @@ public class ChannelGroup {
     }
 
     public ChannelGroup(String title, String iconUrl, List<Channel> channels) {
-        this(Helpers.getRandomNumber(1_000, Integer.MAX_VALUE), title, iconUrl, channels);
+        this(Helpers.getRandomNumber(ChannelGroupService.SUBSCRIPTION_GROUP_ID + 100, Integer.MAX_VALUE), title, iconUrl, channels);
     }
 
     public ChannelGroup(int id, String title, String iconUrl, List<Channel> channels) {
