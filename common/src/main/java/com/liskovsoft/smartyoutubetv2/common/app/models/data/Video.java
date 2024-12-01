479ba67a150a87a0cfd9d1e8619ff9ad16465c9f
Yuriy Liskov <firsthash@gmail.com>
2024-12-01 02:19:25 +0200

    liked music suggestions fix

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/data/Video.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/data/Video.java
@@ -852,6 +852,7 @@ public final class Video {
 
     public boolean isSectionPlaylistEnabled(Context context) {
         return PlayerTweaksData.instance(context).isSectionPlaylistEnabled() && getGroup() != null &&
-                (playlistId == null || nextMediaItem == null || belongsToSearch()) && (!isRemote || remotePlaylistId == null);
+                (playlistId == null || PLAYLIST_LIKED_MUSIC.equals(playlistId) || nextMediaItem == null || belongsToSearch()) &&
+                    (!isRemote || remotePlaylistId == null);
     }
 }
