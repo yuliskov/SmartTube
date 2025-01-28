709bb958e4def81f58691a01271d08bae65e5b9e
Yuriy Liskov <firsthash@gmail.com>
2025-01-28 18:25:26 +0200

    crash restore upd

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/browse/BrowseFragment.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/browse/BrowseFragment.java
@@ -30,6 +30,7 @@ import com.liskovsoft.smartyoutubetv2.common.app.presenters.PlaybackPresenter;
 import com.liskovsoft.smartyoutubetv2.common.app.presenters.SearchPresenter;
 import com.liskovsoft.smartyoutubetv2.common.app.presenters.SplashPresenter;
 import com.liskovsoft.smartyoutubetv2.common.app.views.BrowseView;
+import com.liskovsoft.smartyoutubetv2.common.app.views.ViewManager;
 import com.liskovsoft.smartyoutubetv2.common.utils.Utils;
 import com.liskovsoft.smartyoutubetv2.tv.R;
 import com.liskovsoft.smartyoutubetv2.tv.presenter.IconHeaderItemPresenter;
@@ -48,6 +49,7 @@ public class BrowseFragment extends BrowseSupportFragment implements BrowseView 
     private static final String TAG = BrowseFragment.class.getSimpleName();
     private static final String SELECTED_HEADER_INDEX = "SelectedHeaderIndex";
     private static final String SELECTED_VIDEO = "SelectedVideo";
+    private static final String IS_PLAYER_IN_FOREGROUND = "IsPlayerInForeground";
     private ArrayObjectAdapter mSectionRowAdapter;
     private BrowsePresenter mBrowsePresenter;
     private Map<Integer, BrowseSection> mSections;
@@ -58,14 +60,18 @@ public class BrowseFragment extends BrowseSupportFragment implements BrowseView 
     private boolean mIsFragmentCreated;
     private int mRestoredHeaderIndex = -1;
     private Video mRestoredVideo;
+    private boolean mRestoredInForeground;
     private boolean mFocusOnContent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(null);
 
-        mRestoredHeaderIndex = savedInstanceState != null ? savedInstanceState.getInt(SELECTED_HEADER_INDEX, -1) : -1;
-        mRestoredVideo = savedInstanceState != null ? Video.fromString(savedInstanceState.getString(SELECTED_VIDEO)) : null;
+        if (savedInstanceState != null) {
+            mRestoredHeaderIndex = savedInstanceState.getInt(SELECTED_HEADER_INDEX, -1);
+            mRestoredVideo = Video.fromString(savedInstanceState.getString(SELECTED_VIDEO));
+            mRestoredInForeground = savedInstanceState.getBoolean(IS_PLAYER_IN_FOREGROUND, false);
+        }
         mIsFragmentCreated = true;
 
         mSections = new HashMap<>();
@@ -89,6 +95,7 @@ public class BrowseFragment extends BrowseSupportFragment implements BrowseView 
         outState.putInt(SELECTED_HEADER_INDEX, getSelectedPosition());
         if (mBrowsePresenter.getCurrentVideo() != null) {
             outState.putString(SELECTED_VIDEO, mBrowsePresenter.getCurrentVideo().toString());
+            outState.putBoolean(IS_PLAYER_IN_FOREGROUND, ViewManager.instance(getContext()).isPlayerInForeground());
         }
     }
 
@@ -119,7 +126,7 @@ public class BrowseFragment extends BrowseSupportFragment implements BrowseView 
 
             // Restore state after crash
             selectSectionItem(mRestoredVideo);
-            if (PlaybackPresenter.instance(getContext()).getPlayer() == null) {
+            if (PlaybackPresenter.instance(getContext()).getPlayer() == null && mRestoredInForeground) {
                 PlaybackPresenter.instance(getContext()).openVideo(mRestoredVideo);
             }
             mRestoredVideo = null;
