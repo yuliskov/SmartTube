84eaa031969f3daa4c3b854c53ca580c56d9e28f
Yuriy Liskov <firsthash@gmail.com>
2025-04-04 16:47:59 +0300

    crash fix

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/webbrowser/WebBrowserActivity.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/webbrowser/WebBrowserActivity.java
@@ -1,5 +1,6 @@
 package com.liskovsoft.smartyoutubetv2.tv.ui.webbrowser;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import com.liskovsoft.smartyoutubetv2.tv.R;
 import com.liskovsoft.smartyoutubetv2.tv.ui.common.LeanbackActivity;
@@ -12,7 +13,7 @@ public class WebBrowserActivity extends LeanbackActivity {
         super.onCreate(savedInstanceState);
         try {
             setContentView(R.layout.fragment_webbrowser);
-        } catch (ClassCastException e) { // WebBrowserFragment is not a Fragment
+        } catch (Fragment.InstantiationException e) { // WebBrowserFragment is not a Fragment
             e.printStackTrace();
             finishReally();
         }
