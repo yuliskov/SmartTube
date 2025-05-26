5e4a4712cf6796b6c81cbc60342d3287c0319fec
Yuriy Liskov <firsthash@gmail.com>
2025-05-26 10:58:41 +0300

    revert previous fix

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/webbrowser/WebBrowserActivity.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/webbrowser/WebBrowserActivity.java
@@ -2,7 +2,6 @@ package com.liskovsoft.smartyoutubetv2.tv.ui.webbrowser;
 
 import android.os.Bundle;
 
-import com.liskovsoft.sharedutils.mylogger.Log;
 import com.liskovsoft.smartyoutubetv2.tv.R;
 import com.liskovsoft.smartyoutubetv2.tv.ui.common.LeanbackActivity;
 
@@ -12,13 +11,6 @@ public class WebBrowserActivity extends LeanbackActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Check if the fragment class is actually a Fragment
-        if (!androidx.fragment.app.Fragment.class.isAssignableFrom(WebBrowserFragment.class)) {
-            Log.e(TAG, "WebBrowserFragment is not a Fragment. Skipping setContentView.");
-            finish();
-            return;
-        }
 
         // WebBrowserFragment is not a Fragment: ClassCastException or Fragment.InstantiationException
         setContentView(R.layout.fragment_webbrowser);
