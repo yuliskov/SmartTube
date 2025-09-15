43e1d78a203b965ea62323ea7d13009347e15014
Yuriy Liskov <firsthash@gmail.com>
2025-09-15 18:12:02 +0300

    fix player white bg?

---
+++ exoplayer-amzn-2.10.6/library/sabr/src/main/java/com/google/android/exoplayer2/source/sabr/parser/ResponseParser.java
@@ -0,0 +1,11 @@
+package com.google.android.exoplayer2.source.sabr.parser;
+
+import com.google.android.exoplayer2.extractor.ExtractorInput;
+
+public class ResponseParser {
+    private final ExtractorInput extractorInput;
+
+    public ResponseParser(ExtractorInput extractorInput) {
+        this.extractorInput = extractorInput;
+    }
+}
---
+++ exoplayer-amzn-2.10.6/library/sabr/src/main/java/com/google/android/exoplayer2/source/sabr/parser/ump/UMPDecoder.java
@@ -0,0 +1,11 @@
+package com.google.android.exoplayer2.source.sabr.parser.ump;
+
+import com.google.android.exoplayer2.extractor.ExtractorInput;
+
+public class UMPDecoder {
+    private final ExtractorInput extractorInput;
+
+    public UMPDecoder(ExtractorInput extractorInput) {
+        this.extractorInput = extractorInput;
+    }
+}
