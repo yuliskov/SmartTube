e4b6755a2492a44a4ae93c7a34dea2ef1d3166c9
Yuriy Liskov <firsthash@gmail.com>
2025-10-17 19:20:33 +0300

    exoplayer: upd sabr classes

--- exoplayer-amzn-2.10.6/library/sabr/src/main/java/com/google/android/exoplayer2/source/sabr/parser/SabrExtractor.java
+++ exoplayer-amzn-2.10.6/library/sabr/src/main/java/com/google/android/exoplayer2/source/sabr/parser/SabrExtractor.java
@@ -5,10 +5,18 @@ import android.util.SparseArray;
 import androidx.annotation.IntDef;
 
 import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.drm.DrmInitData;
 import com.google.android.exoplayer2.extractor.Extractor;
 import com.google.android.exoplayer2.extractor.ExtractorInput;
 import com.google.android.exoplayer2.extractor.ExtractorOutput;
 import com.google.android.exoplayer2.extractor.PositionHolder;
+import com.google.android.exoplayer2.extractor.TrackOutput;
+import com.google.android.exoplayer2.source.sabr.parser.parts.FormatInitializedSabrPart;
+import com.google.android.exoplayer2.source.sabr.parser.parts.MediaSegmentDataSabrPart;
+import com.google.android.exoplayer2.source.sabr.parser.parts.MediaSegmentEndSabrPart;
+import com.google.android.exoplayer2.source.sabr.parser.parts.MediaSegmentInitSabrPart;
+import com.google.android.exoplayer2.source.sabr.parser.parts.SabrPart;
 import com.google.android.exoplayer2.util.LongArray;
 import com.google.android.exoplayer2.util.NalUnitUtil;
 import com.google.android.exoplayer2.util.ParsableByteArray;
@@ -167,8 +175,30 @@ public class SabrExtractor implements Extractor {
     @Override
     public int read(ExtractorInput input, PositionHolder seekPosition)
             throws IOException, InterruptedException {
-        // TODO: not implemented
-        return Extractor.RESULT_END_OF_INPUT;
+        sampleRead = false;
+        boolean continueReading = true;
+        while (continueReading && !sampleRead) {
+            SabrPart sabrPart = sabrStream.parse(input);
+            continueReading = sabrPart != null;
+
+            if (sabrPart instanceof FormatInitializedSabrPart) {
+                // TODO: not initialized
+            } else if (sabrPart instanceof MediaSegmentInitSabrPart) {
+                // TODO: not initialized
+            } else if (sabrPart instanceof MediaSegmentDataSabrPart) {
+                // TODO: not initialized
+            } else if (sabrPart instanceof MediaSegmentEndSabrPart) {
+                // TODO: not initialized
+            }
+
+            if (continueReading && maybeSeekForCues(seekPosition, input.getPosition())) {
+                return Extractor.RESULT_SEEK;
+            }
+        }
+        if (!continueReading) {
+            return Extractor.RESULT_END_OF_INPUT;
+        }
+        return Extractor.RESULT_CONTINUE;
     }
 
     @Override
@@ -202,7 +232,107 @@ public class SabrExtractor implements Extractor {
         sampleStrippedBytes.reset();
     }
 
+    /**
+     * Updates the position of the holder to Cues element's position if the extractor configuration
+     * permits use of master seek entry. After building Cues sets the holder's position back to where
+     * it was before.
+     *
+     * @param seekPosition The holder whose position will be updated.
+     * @param currentPosition Current position of the input.
+     * @return Whether the seek position was updated.
+     */
+    private boolean maybeSeekForCues(PositionHolder seekPosition, long currentPosition) {
+        if (seekForCues) {
+            seekPositionAfterBuildingCues = currentPosition;
+            seekPosition.position = cuesContentPosition;
+            seekForCues = false;
+            return true;
+        }
+        // After parsing Cues, seek back to original position if available. We will not do this unless
+        // we seeked to get to the Cues in the first place.
+        if (sentSeekMap && seekPositionAfterBuildingCues != C.POSITION_UNSET) {
+            seekPosition.position = seekPositionAfterBuildingCues;
+            seekPositionAfterBuildingCues = C.POSITION_UNSET;
+            return true;
+        }
+        return false;
+    }
+
+    private void commitSampleToOutput(Track track, long timeUs) {
+        track.output.sampleMetadata(timeUs, blockFlags, sampleBytesWritten, 0, track.cryptoData);
+        sampleRead = true;
+        resetSample();
+    }
+
     private static final class Track {
-        // TODO: implement track
+        private static final int DISPLAY_UNIT_PIXELS = 0;
+        private static final int MAX_CHROMATICITY = 50000;  // Defined in CTA-861.3.
+        /**
+         * Default max content light level (CLL) that should be encoded into hdrStaticInfo.
+         */
+        private static final int DEFAULT_MAX_CLL = 1000;  // nits.
+
+        /**
+         * Default frame-average light level (FALL) that should be encoded into hdrStaticInfo.
+         */
+        private static final int DEFAULT_MAX_FALL = 200;  // nits.
+
+        // Common elements.
+        public String name;
+        public String codecId;
+        public int number;
+        public int type;
+        public int defaultSampleDurationNs;
+        public boolean hasContentEncryption;
+        public byte[] sampleStrippedBytes;
+        public TrackOutput.CryptoData cryptoData;
+        public byte[] codecPrivate;
+        public DrmInitData drmInitData;
+
+        // Video elements.
+        public int width = Format.NO_VALUE;
+        public int height = Format.NO_VALUE;
+        public int displayWidth = Format.NO_VALUE;
+        public int displayHeight = Format.NO_VALUE;
+        public int displayUnit = DISPLAY_UNIT_PIXELS;
+        @C.Projection public int projectionType = Format.NO_VALUE;
+        public float projectionPoseYaw = 0f;
+        public float projectionPosePitch = 0f;
+        public float projectionPoseRoll = 0f;
+        public byte[] projectionData = null;
+        @C.StereoMode
+        public int stereoMode = Format.NO_VALUE;
+        public boolean hasColorInfo = false;
+        @C.ColorSpace
+        public int colorSpace = Format.NO_VALUE;
+        @C.ColorTransfer
+        public int colorTransfer = Format.NO_VALUE;
+        @C.ColorRange
+        public int colorRange = Format.NO_VALUE;
+        public int maxContentLuminance = DEFAULT_MAX_CLL;
+        public int maxFrameAverageLuminance = DEFAULT_MAX_FALL;
+        public float primaryRChromaticityX = Format.NO_VALUE;
+        public float primaryRChromaticityY = Format.NO_VALUE;
+        public float primaryGChromaticityX = Format.NO_VALUE;
+        public float primaryGChromaticityY = Format.NO_VALUE;
+        public float primaryBChromaticityX = Format.NO_VALUE;
+        public float primaryBChromaticityY = Format.NO_VALUE;
+        public float whitePointChromaticityX = Format.NO_VALUE;
+        public float whitePointChromaticityY = Format.NO_VALUE;
+        public float maxMasteringLuminance = Format.NO_VALUE;
+        public float minMasteringLuminance = Format.NO_VALUE;
+
+        // Audio elements. Initially set to their default values.
+        public int channelCount = 1;
+        public int audioBitDepth = Format.NO_VALUE;
+        public int sampleRate = 8000;
+        public long codecDelayNs = 0;
+        public long seekPreRollNs = 0;
+
+        // Set when the output is initialized. nalUnitLengthFieldLength is only set for H264/H265.
+        public TrackOutput output;
+        public int nalUnitLengthFieldLength;
+
+        // TODO: not implemented
     }
 }
