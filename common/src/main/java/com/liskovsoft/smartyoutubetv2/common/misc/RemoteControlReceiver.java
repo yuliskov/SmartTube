f753d822ae2dfdc0df171b40121fb4ebede5460d
Yuriy Liskov <firsthash@gmail.com>
2025-12-05 02:07:21 +0200

    remote control service fix on Android 12+

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/RemoteControlReceiver.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/RemoteControlReceiver.java
@@ -3,6 +3,8 @@ package com.liskovsoft.smartyoutubetv2.common.misc;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build.VERSION;
+
 import com.liskovsoft.sharedutils.mylogger.Log;
 import com.liskovsoft.smartyoutubetv2.common.utils.Utils;
 
@@ -11,10 +13,13 @@ public class RemoteControlReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(TAG, "Initializing remote control listener...");
+        // Starting from Android 12 foreground service not supported
+        if (VERSION.SDK_INT < 31) {
+            Log.d(TAG, "Initializing remote control listener...");
 
-        // Fix unload from the memory on some devices?
-        Utils.updateRemoteControlService(context);
+            // Fix unload from the memory on some devices?
+            Utils.updateRemoteControlService(context);
+        }
 
         //Utils.startRemoteControlWorkRequest(context);
 
