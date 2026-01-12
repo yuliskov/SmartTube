d5f1b6d2b71d7e10de53af2d97e649982ea96d02
Maruno Ulfdrengr <maruno@ulfdrengr.nl>
2026-01-12 17:15:48 +0000

    Fix up/down skipping in normal videos
    It seems up/down accidentally skips normal video from the new shorts video, see: https://github.com/yuliskov/SmartTube/issues/5294
    
    This extra check should prevent the up/down skip handler from acting on normal videos

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/controllers/PlayerUIController.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/app/models/playback/controllers/PlayerUIController.java
@@ -762,7 +762,8 @@ public class PlayerUIController extends BasePlayerController {
 
     private boolean handleUpDownSkip(int keyCode) {
         if (getPlayer() == null || getPlayer().isOverlayShown() || getVideo() == null ||
-                getVideo().belongsToShortsGroup() && !getPlayerTweaksData().isQuickSkipShortsAltEnabled()) {
+                (getVideo().belongsToShortsGroup() && !getPlayerTweaksData().isQuickSkipShortsAltEnabled()) ||
+                !getVideo().belongsToShortsGroup()) {
             return false;
         }
 
@@ -1099,3 +1100,4 @@ public class PlayerUIController extends BasePlayerController {
         ChannelPresenter.instance(getContext()).openChannel(getVideo());
     }
 }
+
