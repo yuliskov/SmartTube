fc1f7d7e62fc515c317ac7a4b806fc91f9937e49
Jaiden D <62959173+WyvrnRes@users.noreply.github.com>
2026-02-28 20:17:32 +0000

    first changes

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/presenter/SettingsCardPresenter.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/presenter/SettingsCardPresenter.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import androidx.core.content.ContextCompat;
 import androidx.leanback.widget.Presenter;
 import com.liskovsoft.sharedutils.helpers.Helpers;
 import com.liskovsoft.smartyoutubetv2.common.app.models.data.SettingsItem;
@@ -15,38 +14,16 @@ import com.liskovsoft.smartyoutubetv2.tv.R;
 import com.liskovsoft.smartyoutubetv2.tv.util.ViewUtil;
 
 public class SettingsCardPresenter extends Presenter {
-    private int mDefaultBackgroundColor;
-    private int mDefaultTextColor;
-    private int mSelectedBackgroundColor;
-    private int mSelectedTextColor;
-
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent) {
         Context context = parent.getContext();
 
-        mDefaultBackgroundColor =
-                ContextCompat.getColor(context, Helpers.getThemeAttr(context, R.attr.cardDefaultBackground));
-        mDefaultTextColor =
-                ContextCompat.getColor(context, R.color.card_default_text);
-        mSelectedBackgroundColor =
-                ContextCompat.getColor(context, R.color.card_selected_background_white);
-        mSelectedTextColor =
-                ContextCompat.getColor(context, R.color.card_selected_text_grey);
+    View container = LayoutInflater.from(context).inflate(R.layout.settings_card, null);
+    container.setBackgroundResource(R.drawable.settings_card_background);
 
-        View container = LayoutInflater.from(context).inflate(R.layout.settings_card, null);
-        container.setBackgroundColor(mDefaultBackgroundColor);
-
-        TextView textView = container.findViewById(R.id.settings_title);
-        textView.setBackgroundColor(mDefaultBackgroundColor);
-        textView.setTextColor(mDefaultTextColor);
+    TextView textView = container.findViewById(R.id.settings_title);
 
         container.setOnFocusChangeListener((v, hasFocus) -> {
-            int backgroundColor = hasFocus ? mSelectedBackgroundColor : mDefaultBackgroundColor;
-            int textColor = hasFocus ? mSelectedTextColor : mDefaultTextColor;
-            
-            textView.setBackgroundColor(backgroundColor);
-            textView.setTextColor(textColor);
-
             if (hasFocus) {
                 ViewUtil.enableMarquee(textView);
                 ViewUtil.setTextScrollSpeed(textView, MainUIData.instance(context).getCardTextScrollSpeed());
