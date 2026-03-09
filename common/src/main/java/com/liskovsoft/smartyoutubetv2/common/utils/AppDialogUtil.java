21268e5fa72da16a69701838d2e164963b699451
chouyu <chouyu.8ga@gmail.com>
2026-03-10 02:09:01 +0800

    Update AppDialogUtil.java
    Change audio shift range from -8s~+8s to -2s~+2s with 5ms step)

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/utils/AppDialogUtil.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/utils/AppDialogUtil.java
@@ -409,7 +409,7 @@ public class AppDialogUtil {
 
         List<OptionItem> options = new ArrayList<>();
 
-        for (int delayMs : Helpers.range(-8_000, 8_000, 50)) {
+        for (int delayMs : Helpers.range(-2_000, 2_000, 5)) {
             options.add(UiOptionItem.from(context.getString(R.string.audio_shift_sec, Helpers.toString(delayMs / 1_000f)),
                     optionItem -> {
                         playerData.setAudioDelayMs(delayMs);
@@ -1145,3 +1145,4 @@ public class AppDialogUtil {
         settingsPresenter.showDialog(playbackQueueCategoryTitle);
     }
 }
+
