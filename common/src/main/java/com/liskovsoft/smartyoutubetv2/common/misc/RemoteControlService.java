fbddb8793d89a70134795eea20efeb15bd341ebe
Yuriy Liskov <firsthash@gmail.com>
2026-03-16 19:07:23 +0200

    RemoteControlService: Fix ForegroundServiceStartNotAllowedException

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/RemoteControlService.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/RemoteControlService.java
@@ -5,7 +5,11 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Build.VERSION;
 import android.os.IBinder;
+import android.support.v4.media.session.MediaSessionCompat;
+
 import androidx.annotation.Nullable;
+
+import com.liskovsoft.sharedutils.helpers.AppInfoHelpers;
 import com.liskovsoft.sharedutils.helpers.Helpers;
 import com.liskovsoft.sharedutils.mylogger.Log;
 import com.liskovsoft.smartyoutubetv2.common.R;
@@ -29,17 +33,16 @@ public class RemoteControlService extends Service {
     public void onCreate() {
         super.onCreate();
 
-        //// https://stackoverflow.com/questions/46445265/android-8-0-java-lang-illegalstateexception-not-allowed-to-start-service-inten
-        //if (VERSION.SDK_INT >= 26) {
-        //    // NOTE: it's impossible to hide notification on Android 9 and above
-        //    // https://stackoverflow.com/questions/10962418/how-to-startforeground-without-showing-notification
-        //    startForeground(NOTIFICATION_ID, createNotification());
-        //}
-
         // https://stackoverflow.com/questions/46445265/android-8-0-java-lang-illegalstateexception-not-allowed-to-start-service-inten
         // NOTE: it's impossible to hide notification on Android 9 and above
         // https://stackoverflow.com/questions/10962418/how-to-startforeground-without-showing-notification
         try {
+            if (AppInfoHelpers.getRealSdkVersion(this) >= 34) { // Fix ForegroundServiceStartNotAllowedException
+                // Tie service to active media session
+                MediaSessionCompat mediaSession = new MediaSessionCompat(this, TAG);
+                mediaSession.setActive(true);
+            }
+
             startForeground(NOTIFICATION_ID, createNotification());
         } catch (NullPointerException e) {
             // NullPointerException: Attempt to read from field 'int com.android.server.am.UidRecord.curProcState' on a null object reference
