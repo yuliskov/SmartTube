1b9375a14b3d1c7c6bdf65538e259585456ddaa0
Yuriy Liskov <firsthash@gmail.com>
2026-03-17 21:08:14 +0200

    web browser: fragment instantiation fix

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/webbrowser/WebBrowserActivity.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/webbrowser/WebBrowserActivity.java
@@ -2,6 +2,7 @@ package com.liskovsoft.smartyoutubetv2.tv.ui.webbrowser;
 
 import android.os.Bundle;
 
+import com.liskovsoft.sharedutils.helpers.MessageHelpers;
 import com.liskovsoft.smartyoutubetv2.tv.R;
 import com.liskovsoft.smartyoutubetv2.tv.ui.common.LeanbackActivity;
 
@@ -12,7 +13,15 @@ public class WebBrowserActivity extends LeanbackActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Caused by android.app.Fragment$InstantiationException
+        // Trying to instantiate a class com.liskovsoft.smartyoutubetv2.tv.ui.webbrowser.WebBrowserFragment that is not a Fragment
         // WebBrowserFragment is not a Fragment: ClassCastException or Fragment.InstantiationException
-        setContentView(R.layout.fragment_webbrowser);
+        try {
+            setContentView(R.layout.fragment_webbrowser);
+        } catch (Exception e) {
+            e.printStackTrace();
+            MessageHelpers.showMessage(this, e.getMessage());
+            finish();
+        }
     }
 }
