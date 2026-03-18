df4e87a6e414e26c245e86c548c65d9a1a0b0f9f
Yuriy Liskov <firsthash@gmail.com>
2026-03-18 19:01:56 +0200

    RemoveController: fix foreground service not allowed

--- common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/RemoteControlReceiver.java
+++ common/src/main/java/com/liskovsoft/smartyoutubetv2/common/misc/RemoteControlReceiver.java
@@ -1,5 +1,6 @@
 package com.liskovsoft.smartyoutubetv2.common.misc;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +11,7 @@ import com.liskovsoft.smartyoutubetv2.common.utils.Utils;
 public class RemoteControlReceiver extends BroadcastReceiver {
     private static final String TAG = RemoteControlReceiver.class.getSimpleName();
 
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "Initializing remote control listener...");
@@ -17,11 +19,11 @@ public class RemoteControlReceiver extends BroadcastReceiver {
         // Fix unload from the memory on some devices?
         // NOTE: Starting from Android 12 (api 31) foreground service with type 'connectedDevice' not supported
         // Use 'mediaPlayback' type instead
-        Utils.updateRemoteControlService(context);
-
-        //Utils.startRemoteControlWorkRequest(context);
-
-        // Couldn't success inside periodic work request
-        //PlaybackPresenter.instance(context); // init RemoteControlListener
+        try {
+            Utils.updateRemoteControlService(context);
+        } catch (Exception e) {
+            // ForegroundServiceStartNotAllowedException: startForegroundService() not allowed due to mAllowStartForeground false (Android 12)
+            e.printStackTrace();
+        }
     }
 }
