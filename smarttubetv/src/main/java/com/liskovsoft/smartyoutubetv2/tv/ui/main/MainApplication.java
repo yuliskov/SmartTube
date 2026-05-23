036804aaeb70c44a9c2a691aa466b267726373e0
Yuriy Liskov <firsthash@gmail.com>
2026-05-23 17:08:27 +0300

    crash fixes; upd SharedModules

--- smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/main/MainApplication.java
+++ smarttubetv/src/main/java/com/liskovsoft/smartyoutubetv2/tv/ui/main/MainApplication.java
@@ -2,6 +2,7 @@ package com.liskovsoft.smartyoutubetv2.tv.ui.main;
 
 import androidx.multidex.MultiDexApplication;
 
+import com.liskovsoft.sharedutils.helpers.AppInfoHelpers;
 import com.liskovsoft.sharedutils.helpers.Helpers;
 import com.liskovsoft.smartyoutubetv2.common.app.models.data.BrowseSection;
 import com.liskovsoft.smartyoutubetv2.common.app.presenters.BrowsePresenter;
@@ -56,7 +57,9 @@ public class MainApplication extends MultiDexApplication { // fix: Didn't find c
         //if (Build.VERSION.SDK_INT < 29) {
         //    Security.insertProviderAt(Conscrypt.newProvider(), 1);
         //}
-        Security.insertProviderAt(Conscrypt.newProvider(), 1);
+        if (isConscryptSupported()) {
+            Security.insertProviderAt(Conscrypt.newProvider(), 1);
+        }
 
         setupGlobalExceptionHandler();
         setupViewManager();
@@ -98,10 +101,14 @@ public class MainApplication extends MultiDexApplication { // fix: Didn't find c
     }
 
     private boolean shouldIgnore(Throwable e) {
-        if (Helpers.contains(e.getMessage(), "KatnissVoiceInteractionService")) {
+        if (Helpers.containsAny(e.getMessage(), "KatnissVoiceInteractionService", "ListenableFuture")) {
             // IllegalStateException: Not allowed to start service Intent { act=android.service.voice.VoiceInteractionService
             // cmp=com.google.android.katniss/.search.serviceapi.KatnissVoiceInteractionService (has extras) }:
             // app is in background uid UidRecord{40e7240 u0a19 CEM idle change:cached procs:1 seq(0,0,0)}
+
+            // java.lang.NoSuchMethodError: No interface method addListener(Ljava/lang/Runnable;Ljava/util/concurrent/Executor;)V
+            // in class Lcom/google/common/util/concurrent/ListenableFuture; or its super classes
+            // (declaration of 'com.google.common.util.concurrent.ListenableFuture' appears in /system/framework/libsetting.jar)
             return true;
         }
 
@@ -141,5 +148,9 @@ public class MainApplication extends MultiDexApplication { // fix: Didn't find c
                 }
             }
         }
+    }
+
+    private boolean isConscryptSupported() {
+        return !Helpers.equalsAny(Helpers.getCrashlyticsDeviceName(), "Smart (Cvte)");
     }
 }
